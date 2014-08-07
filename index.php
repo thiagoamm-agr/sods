60d314c4bcc8f1f81793796befeaa88f31d68b47
edward-agr <edwardneto13@gmail.com>
2014-08-07 17:04:03 -0300

    Commit de atualização da página de login utilizando o bootstrap

--- index.php
+++ index.php
@@ -5,66 +5,35 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <link rel="stylesheet" type="text/css" media="screen" href="css/normalize.css" />
-        <style type="text/css">
-            h1 {
-                text-align: center;
-            }
-
-            body {
-                background-color: #DDD;
-                font-size: 12pt;
-                font-family: Arial;
-            }
-
-            form {
-                margin: 8% auto 8%;
-                width: 13%;
-            }
-
-            legend {
-                font-weight: bold;
-                font-size: 14pt;
-            }
-
-            fieldset {
-                padding-left: 15px;
-            }
-
-            input[type="text"] {
-                width: auto;
-            }
-
-            p > input[type="submit"] {
-                float: right;
-                /*margin-right: 4px;*/
-            }
-        </style>
-        <title>SODS - Sistema de Solicitação de Desenvolvimento de Software</title>
+        <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap/bootstrap.css" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">	
+	    
+	    <!-- Estilo para esse template -->
+	    <link href="css/bootstrap/signin.css" rel="stylesheet">
+	    
+	    <title>Login</title>
     </head>
-    <body>
-        <h1>SODS - Sistema de Solicitação de Desenvolvimento de Software</h1>
-        <form id="form_login" name="form_login" method="POST">
-            <fieldset>
-                <legend>SODS</legend>
-                <p>
-                    <label for="login">Login:</label>
-                    <input id="login" name="login" type="text" />
-                </p>
-                <p>
-                    <label for="senha">Senha:</label>
-                    <input id="senha" name="senha" type="password" />
-                </p>
-                <p>
-                    <input type="submit" value="Entrar" />
-                </p>
-            </fieldset>
-        </form>
+    	<body>
+        	<center><h2>SODS - Sistema de Solicitação de Desenvolvimento de Software</h2></center>
+        		<div class="container">
+			      <form class="form-signin" role="form" method="post" id="formlogin" name="formlogin">
+			        <h3 class="form-signin-heading">Login</h3>
+			        <input type="login" class="form-control" placeholder="Login" name="login" id="login" required autofocus>
+			        <input type="password" class="form-control" placeholder="Senha" name="senha" id="senha" required>
+			        <div class="checkbox">
+			          <label>
+			            <input type="checkbox" value="remember-me"> Lembre-me
+			          </label>
+			        </div>
+			        <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+			      </form>
+    			</div> 
 <?php
     @require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/lib/user.php';
             
-    $login = $_POST['login'];
-    $senha = $_POST['senha'];
+    @$login = $_POST['login'];
+    @$senha = $_POST['senha'];
 
     if (autenticar_usuario($login, $senha)) {        
         header('location: /sods/admin');
