51f2f262042d72f121522d0c799b527b7bb84205
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-08-12 09:09:09 -0300

    Alinhamento à direita do botão de envio do formulário.

--- admin/addRequest.php
+++ admin/addRequest.php
@@ -1,12 +1,12 @@
 <?php
-@session_start ();
+	@session_start ();
 
-require_once $_SERVER ['DOCUMENT_ROOT'] . '/sods/lib/session.php';
+	require_once $_SERVER ['DOCUMENT_ROOT'] . '/sods/lib/session.php';
 
-validar_acesso ();
+	validar_acesso ();
 
-date_default_timezone_set("Brazil/East");
-$data = date("d/m/y " . " H:i");
+	date_default_timezone_set("Brazil/East");
+	$data = date("d/m/y " . " H:i");
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
@@ -23,86 +23,87 @@ $data = date("d/m/y " . " H:i");
 		
 		<title>SODS</title>
 	</head>
-    <body>
-    <?php
-		include '../admin/index.php';
+	<body>
+<?php
+	include '../admin/index.php';
 		
-		require_once $_SERVER ['DOCUMENT_ROOT'] . '/sods/lib/db.php';
-		get_db_connection();
+	require_once $_SERVER ['DOCUMENT_ROOT'] . '/sods/lib/db.php';
 		
-		$query= "select * from tipo_solicitacao";
-		$result = mysql_query($query);
-	?>
-		<form class="form-horizontal" role="form">
-		  
-			  <div class="form-group">
-			    <label for="nome_sol" class="col-sm-2 control-label">Nome</label>
-			    <div class="col-sm-10">
-			      <input type="text" class="form-control" id="nome_sol" placeholder="<?php echo $_SESSION['usuario']['nome_sol'];?>" readonly>
-			    </div>
-			  </div>
+	get_db_connection();
+		
+	$query= "select * from tipo_solicitacao";
+	$result = mysql_query($query);
+?>
+		<form class="form-horizontal" role="form">			  
+			<div class="form-group">
+		    	<label for="nome_sol" class="col-sm-2 control-label">Nome</label>
+		    	<div class="col-sm-10">
+		      		<input type="text" class="form-control" id="nome_sol" placeholder="<?php echo $_SESSION['usuario']['nome_sol'];?>" readonly>
+		    	</div>
+		  	</div>
 			  
-			  <div class="form-group">
-			    <label for="nome_sec" class="col-sm-2 control-label">Lotação</label>
-			    <div class="col-sm-10">
-			      <input type="text" class="form-control" id="nome_sec" placeholder="<?php echo $_SESSION['usuario']['nome_sec'];?>" readonly>
-			    </div>
-			  </div>
+		  	<div class="form-group">
+		    	<label for="nome_sec" class="col-sm-2 control-label">Lotação</label>
+		    	<div class="col-sm-10">
+		      		<input type="text" class="form-control" id="nome_sec" placeholder="<?php echo $_SESSION['usuario']['nome_sec'];?>" readonly>
+		    	</div>
+		  	</div>
 			  
-			  <div class="form-group">
-			  	<label for="descricao" class="col-sm-2 control-label">Descrição</label>
-			  	<div class="col-sm-10">
-			  		<textarea class="form-control" rows="6" placeholder="Digite aqui a descrição detalhada do sistema"></textarea>
-			  	</div>
-			  </div>
+		  	<div class="form-group">
+		  		<label for="descricao" class="col-sm-2 control-label">Descrição</label>
+		  		<div class="col-sm-10">
+		  			<textarea class="form-control" rows="6" placeholder="Digite aqui a descrição detalhada do sistema"></textarea>
+		  		</div>
+		  	</div>
 			  
-			  <div class="form-group">
-			  	<label for="info_adc" class="col-sm-2 control-label">Inf. Adicionais</label>
-			  	<div class="col-sm-10">
-			  		<textarea class="form-control" rows="4" placeholder="Digite aqui alguma informação que julgue importante"></textarea>
-			  		<span class="help-block">Ex.: No desenvolvimento do sistema deveremos contar com um profissional de Direito.</span>
-			  	</div>
-			  </div>
+		  	<div class="form-group">
+		  		<label for="info_adc" class="col-sm-2 control-label">Inf. Adicionais</label>
+		  		<div class="col-sm-10">
+		  			<textarea class="form-control" rows="4" placeholder="Digite aqui alguma informação que julgue importante"></textarea>
+		  			<span class="help-block">Ex.: No desenvolvimento do sistema deveremos contar com um profissional de Direito.</span>
+		  		</div>
+		  	</div>
 			  
-			  <div class="form-group">
-			  	<label for="obs" class="col-sm-2 control-label">Observações</label>
-			  	<div class="col-sm-10">
-			  		<textarea class="form-control" rows="2" placeholder="Observações relevantes para o desenvolvimento"></textarea>
-			  		<span class="help-block">Ex.: Torna-se importante o desenvolvimento por ...</span>
-			  	</div>
-			  </div>
+			<div class="form-group">
+		  		<label for="obs" class="col-sm-2 control-label">Observações</label>
+		  		<div class="col-sm-10">
+		  			<textarea class="form-control" rows="2" placeholder="Observações relevantes para o desenvolvimento"></textarea>
+		  			<span class="help-block">Ex.: Torna-se importante o desenvolvimento por ...</span>
+		  		</div>
+		  	</div>
 			  
-			  <div class="form-group">
-			  	<label for="tipo_sol" class="col-sm-2 control label">Tipo de Solicitação</label>
-			  	<div class="col-sm-10">
-			  		<select class="form-control" name=tipo_sol>
-			  			<?php 
-			  				while($resultFim = mysql_fetch_assoc($result)){
-							?>
+		  	<div class="form-group">
+		  		<label for="tipo_sol" class="col-sm-2 control label">Tipo de Solicitação</label>
+		  		<div class="col-sm-10">
+					<select class="form-control" name=tipo_sol>
+<?php 
+						while($resultFim = mysql_fetch_assoc($result)){
+?>
 							<option value="<?=$resultFim["id"]?>"> <?=$resultFim["nome"]?></option>
-						<?php 
-			  				}
-							?>
+<?php 
+						}
+?>
 					</select>			  		
-			  	</div>
+				</div>						  
+			</div>
 			  
-			  </div>
+		  	<div class="form-group">
+		    	<label for="data" class="col-sm-2 control-label">Data</label>
+		    	<div class="col-sm-10">
+					<input type="text" class="form-control" id="data" placeholder=" <?php echo $data?>" readonly>
+		    	</div>
+			</div>
 			  
-			  <div class="form-group">
-			    <label for="data" class="col-sm-2 control-label">Data</label>
-			    <div class="col-sm-10">
-			      <input type="text" class="form-control" id="data" placeholder=" <?php echo $data?>" readonly>
-			    </div>
-			  </div>
-			  
-			  <div class="form-group">
-			    <label for="ult_alteracao" class="col-sm-2 control-label">Ultima Alteração</label>
-			    <div class="col-sm-10">
-			      <input type="text" class="form-control" id="ult_ateracao" placeholder="00/00/00 00:00" readonly>
-			    </div>
-			  </div>
-			  <button type="submit" class="btn btn-primary" >Enviar Solicitação</button>
-		</form>
-	
+		  	<div class="form-group">
+		    	<label for="ult_alteracao" class="col-sm-2 control-label">Ultima Alteração</label>
+		    	<div class="col-sm-10">
+		      		<input type="text" class="form-control" id="ult_ateracao" placeholder="00/00/00 00:00" readonly>
+		    	</div>
+			</div>
+			
+			<button type="submit" class="btn btn-primary pull-right">
+				Enviar Solicitação
+			</button>
+		</form>	
     </body>
 </html>
