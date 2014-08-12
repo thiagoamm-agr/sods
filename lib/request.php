9a10a89e2ff6d163df8a1a44a9e9e709822de1d7
edward-agr <edwardneto13@gmail.com>
2014-08-12 10:21:00 -0300

    Commit: script de inserção de solicitação no bd

---
+++ lib/request.php
@@ -0,0 +1,26 @@
+<?php
+
+@session_start();
+
+date_default_timezone_set("Brazil/East");
+
+@require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/lib/session.php';
+@require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/lib/db.php';
+
+validar_acesso();
+
+get_db_connection();
+
+$solicitante_id = $_SESSION['usuario']['id'];
+$desc = $_POST['desc'];
+$info_add = $_POST['info_add'];
+$obs = $_POST['obs'];
+$tipo_sol = $_POST['tipo_sol'];
+
+$query = mysql_query("insert into solicitacao (solicitante_id, detalhamento, info_adicionais, observacoes, tipo_solicitacao_id) 
+		values ($solicitante_id , '$desc', '$info_add', '$obs', $tipo_sol)") or die(mysql_error());
+
+header('location: /sods/admin/listRequests.php');
+
+ 
+?>
