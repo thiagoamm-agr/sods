7b507f5e8db24b7276f421da6b0c28da3bffbebe
edward-agr <edwardneto13@gmail.com>
2014-08-15 14:54:23 -0300

    Inserção de código para mostrar logo da AGR.

--- admin/index.php
+++ admin/index.php
@@ -2,6 +2,8 @@
 	include $_SERVER ['DOCUMENT_ROOT'] . '/sods/includes/topo.php';
 ?>
 
+<center><img src= "../img/logo_agr.jpg"/></center>
+
 <?php
     include $_SERVER ['DOCUMENT_ROOT'] . '/sods/includes/rodape.php';
 ?>
