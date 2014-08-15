1fb3bd5aaf2c277f43e0297d6c33c72295e37ad6
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-08-15 09:30:09 -0300

    Criado o topo das páginas do projeto.

--- includes/topo.php
+++ includes/topo.php
@@ -4,6 +4,9 @@
 	require_once $_SERVER ['DOCUMENT_ROOT'] . '/sods/lib/session.php';
 
 	validar_acesso();
+	
+	date_default_timezone_set("Brazil/East");
+	$data = date("d/m/y " . " H:i");
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -27,6 +30,8 @@
       		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
+		
+		<link rel="stylesheet" href="/sods/css/table.css" />
 				
 		<title>SODS</title>
     </head>
@@ -41,38 +46,48 @@
 						<span class="icon-bar"></span>
             		</button>
 					<a class="navbar-brand" style="font-size: 40px; color: white;" 
-				   	   href="index.php" title="Sistema para Solicitação de Desenvolvimento de Software">SODS</a>
+				   	   href="/sods/admin/" title="Sistema para Solicitação de Desenvolvimento de Software">SODS</a>
           		</div>
 				<div class="collapse navbar-collapse">
-					<ul class="nav navbar-nav">						
+					<ul class="nav navbar-nav">
+						<li class="dropdown">
+							<a class="dropdown-toggle" href="/sods/admin/">
+								Início
+							</a>
+						</li>
 <?php 
-						if ($_SESSION['usuario']['tipo_usuario'] == 'A') {
-							// Menu do Administrador do Sistema.
-							echo '<li class="dropdown">
-							      	<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-		                              	Cadastro <span class="caret"></span>
-		                          	</a>
-		                		  	<ul class="dropdown-menu" role="menu">
-                                      	<li><a href="../admin/listUsers.php">Usuário</a></li>
-                                      	<li><a href="#">Solicitação</a></li>
-                                  	</ul>
-		              	      	 </li>';
-							echo '<li class="dropdown">
-				                  	<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-				                      	Relatório <span class="caret"></span>
-				                  	</a>
-				                  	<ul class="dropdown-menu" role="menu">
-				                      	<li><a href="#">Usuário</a></li>
-				                      	<li><a href="#">Solicitação</a></li>
-				                  	</ul>
-				              	 </li>';
-							} else {
-                            	// Menu do Usuário do Sistema.
-								echo '<li><a href="../admin/account.php">Conta</a></li>';
-							}
+					if ($_SESSION['usuario']['tipo_usuario'] == 'A') {
+						// Menu do Administrador do Sistema.
+?>							
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+								Cadastros <span class="caret"></span>
+							</a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="/sods/admin/usuarios/">Cadastro de Usuários</a></li>
+								<li><a href="#">Cadastro de Solicitações</a></li>
+							</ul>
+						</li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+								Relatórios <span class="caret"></span>
+							</a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="#">Relatório de Usuários</a></li>
+								<li><a href="#">Relatório de Solicitações</a></li>
+							</ul>
+						</li>
+<?php
+					} else {
+						// Menu do Usuário do Sistema.
 ?>
+						<li><a href="/sods/admin/account.php">Conta</a></li>
+<?php 
+					}
+?>
+						<li class="dropdown"><a class="dropdown-toggle" href="#">Sobre</a></li>
 					</ul>
-            		<span width="100px" />
+            		<span style="width: 100px"></span>
             		<ul class="nav navbar-nav navbar-right">
 						<li>
 							<a href="#">
@@ -84,10 +99,13 @@
 						</li>
               			<li>
               				<form class="navbar-form navbar-right" role="form" action="/sods/lib/logout.php">
-								<button type="submit" class="btn btn-success" href="#">Sair </button>
+								<button type="submit" class="btn btn-success">
+									<strong>Sair</strong>
+								</button>
 							</form>
 						</li>
             		</ul>            		
-				</div><!--/.nav-collapse -->		        
-			</div><!--/.container-fluid -->
+				</div> <!--/.nav-collapse -->		        
+			</div> <!--/.container-fluid -->
 		</div>
+		<div class="container">
