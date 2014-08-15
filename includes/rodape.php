c562edaedbe1c486af0b80203958f0c209fc45d1
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-08-15 09:29:49 -0300

    Criado rodapé das páginas do projeto.

--- includes/rodape.php
+++ includes/rodape.php
@@ -1,7 +1,9 @@
+        </div> <!-- container -->
+        
         <!-- Bootstrap core JavaScript
         ================================================== -->
         <!-- Placed at the end of the document so the pages load faster -->
-        <script src="../js/jquery.min.js"></script>
-        <script src="../js/bootstrap/bootstrap.min.js"></script>
+        <script src="/sods/js/jquery.min.js"></script>
+        <script src="/sods/js/bootstrap/bootstrap.min.js"></script>
     </body>
 </html>
