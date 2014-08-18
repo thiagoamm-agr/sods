370ce12df9135db81ae1615bebc6ff553f7f20dc
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-08-18 15:04:21 -0300

    Alteradas as ocorrências de $_SERVER [] para $_SERVER[].

--- admin/usuarios/index.php
+++ admin/usuarios/index.php
@@ -1,25 +1,10 @@
 <?php
-	include $_SERVER ['DOCUMENT_ROOT'] . '/sods/includes/topo.php';
-			
-	require_once $_SERVER ['DOCUMENT_ROOT'] . '/sods/lib/db.php';
-			
-	get_db_connection();
-		
-	$query = "select " . 
-				"s.id, s.nome, sec.nome as secao, s.cargo, " .
-				"s.fone_ramal, s.login, s.tipo_usuario, s.status " . 
-			"from " . 
-				"solicitante as s " . 
-			"inner join secao as sec " . 
-				"on s.secao_id = sec.id;";
-	
-	$result = mysql_query($query);
-?>		
+	@include $_SERVER['DOCUMENT_ROOT'] . '/sods/includes/topo.php';	
+
+	@require $_SERVER['DOCUMENT_ROOT'] . '/sods/app/controllers/UsuariosController.php';
+?>
+		<div class="container">		
 			<h2>Usuários</h2>
-			<div class="container" >
-					<button class='btn btn-warning btn-sm pull-right' data-toggle='modal' data-target='#modalAdd'><b>Adicionar Usuário</b></button>
-					<br><br>
-			</div>
 			<div class="table-responsive">				
 				<table class="table table-striped table-bordered table-condensed">
 					<thead>
@@ -36,18 +21,23 @@
 	            		</tr>
 					</thead>
 					<tbody>
-<?php		
-					while ($usuario = mysql_fetch_assoc($result)) {
+<?php
+					$controller = new UsuariosController();
+					
+					// Obtém a lista de todos os usuários.
+					$usuarios = $controller->all();
+
+					while ($usuario = mysql_fetch_assoc($usuarios)) {
 ?>
 						<tr>
-							<td><?php echo $usuario['id']?></td>
-							<td><?php echo $usuario['nome']?></td>
-							<td><?php echo $usuario['secao']?></td>
-							<td><?php echo $usuario['cargo']?></td>
-							<td><?php echo $usuario['fone_ramal']?></td>
-							<td><?php echo $usuario['tipo_usuario']?></td>
-							<td><?php echo $usuario['status']?></td>
-							<td><?php echo $usuario['login']?></td>
+							<td><?php echo $usuario['id'] ?></td>
+							<td><?php echo $usuario['nome'] ?></td>
+							<td><?php echo $usuario['secao'] ?></td>
+							<td><?php echo $usuario['cargo'] ?></td>
+							<td><?php echo $usuario['fone_ramal'] ?></td>
+							<td><?php echo $usuario['tipo_usuario'] ?></td>
+							<td><?php echo $usuario['status'] ?></td>
+							<td><?php echo $usuario['login'] ?></td>
 							<td colspan="2">							
 								<button class='btn btn-primary btn-sm' 
 								    data-toggle='modal' data-target='#modalEdit'>
@@ -59,13 +49,13 @@
 							    </button>							
 							</td>
 						</tr>
-<?php 
+<?php
 					} 
 ?>
 					</tbody>
 	    		</table>
     		</div>
-    			    		
+    		    		
 			<!--  Modais -->
 			<div class="modal fade" id="modalEdit" tabindex="-1" role="dialog" aria-labelledby="modalEdit" 
 			    aria-hidden="true">
@@ -150,74 +140,8 @@
     					</form>
     				</div>
     			</div>
-    		</div> 
-    		
-    		<div class="modal fade" id="modalAdd" tabindex="-1" role="dialog" 
-			    aria-labelledby="modalAdd" aria-hidden="true">
-				<div class="modal-dialog">
-					<div class="modal-content">
-						<div class="modal-header">
-							<button type="button" class="close" data-dismiss="modal" aria-hidden="true">
-							    &times;
-							</button>
-							<h4 class="modal-title" id="modalDel">Adicionar Novo Usuário</h4>
-						</div>
-						<div class="modal-body">
-    						<h5>Preencha o Formulário</h5>
-    						 <form role="form" action="#" method="post">
-    							<div class="form-group">
-    								<label for="nome">Nome</label>
-    								<input type="text" class="form-control" name="nome" id="nome"/>    								
-    							</div>
-    							<div class="form-group">
-    								<label for="lotacao">Lotação</label>
-    								<select id="lotacao" name="lotacao" class="form-control">
-<?php 
-										$sql = "select * from secao";
-																
-										$secoes = mysql_query($sql);
-						
-		  								while ($secao = mysql_fetch_assoc($secoes)) {
-?>
-											<option value="<?php echo $secao['id'] ?>">
-												<?php echo $secao['nome']?>
-											</option>
+    		</div> <!--  modais -->
+		</div> <!-- container -->
 <?php
-										} 
-?>
-    								</select>
-    							</div>
-    							<div class="form-group">
-    								<label for="cargo">Cargo</label>
-    								<input type="text" class="form-control" name="cargo" id="cargo"/>
-    							</div>
-    							<div class="form-group">
-    								<label for="fone_ramal">Telefone/Ramal</label>
-    								<input type="text" class="form-control" name="fone-ramal" id="fone-ramal"/>
-    							</div>
-    							<div class="form-group">
-    								<label for="email">Email</label>
-    								<input type="text" class="form-control" name="email" id="email"/>
-    							</div>
-    							<div class="form-group">
-    								<label for="login">login</label>
-    								<input type="text" class="form-control" name="login" id="login"/>
-    							</div>
-    							<div class="form-group">
-    								<input type="checkbox" id="tipo_usr" name="tipo_usr" value="A"> Administrador
-    							</div>
-    						</form>
-    					</div>
-						<form action="#">
-	    					<div class="modal-footer">
-	    						<button type="submit" class="btn btn-success">Salvar</button>
-	    						<button type="button" class="btn btn-primary" data-dismiss="modal">Cancelar</button>	    						
-	    					</div>
-    					</form>
-    				</div>
-    			</div>
-    		</div>     		<!--  Fim Modais -->
-    		    		
-<?php
-	include $_SERVER ['DOCUMENT_ROOT'] . '/sods/includes/rodape.php';
+	@include $_SERVER ['DOCUMENT_ROOT'] . '/sods/includes/rodape.php';
 ?>
