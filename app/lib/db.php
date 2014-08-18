75b0af8f88e0249a4b1754f14350fb20e100ae0a
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-08-18 14:46:48 -0300

    Criação do módulo de acesso ao banco de dados.

---
+++ app/lib/db.php
@@ -0,0 +1,17 @@
+<?php 
+    function get_db_connection($host='10.243.1.14', $user='sods', $password='dev123') {
+        $conexao = null;
+        if (isset($host) && isset($user) && isset($password)) {
+            # Criando a conexão
+            $conexao = mysql_connect($host, $user, $password);
+            if (!isset($conexao)) {
+                die("Não foi possível conectar ao servidor de banco de dados:" . mysql_error());
+            }
+            # Selecionando o banco de dados
+            mysql_select_db('sods_development');    
+            # Informando o charset utilizado.
+            mysql_set_charset('UTF8', $conexao);
+        }
+        return $conexao;
+    }
+?>
