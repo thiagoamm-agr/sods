3bb04024b1cfeb4764cafdcaf56d325fa11590ab
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-08-18 14:55:31 -0300

    Criação da camada de Controle da aplicação.

---
+++ app/controllers/UsuariosController.php
@@ -0,0 +1,28 @@
+<?php
+	@require $_SERVER ['DOCUMENT_ROOT'] . '/sods/app/dao/UsuarioDAO.php';
+	
+	class UsuariosController {
+		
+		private $dao;
+		
+		public function __construct() {
+			$this->dao = new UsuarioDAO();
+		}
+		
+		public function __destruct() {
+			unset($this->dao);
+		}
+		
+		public function __get($field) {
+			return $this->$field;
+		}
+		
+		public function __set($field, $value) {
+			$this->$field = $value;
+		}
+		
+		public function all() {
+			return $this->dao->all();
+		}
+	}
+?>
