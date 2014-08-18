0e62b2c83be87bb19f9eb276645f02f2a85afa5f
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-08-18 15:04:06 -0300

    Alteradas as ocorrências de $_SERVER [] para $_SERVER[].

--- admin/account.php
+++ admin/account.php
@@ -1,65 +1,46 @@
 <?php
-@session_start ();
-
-require_once $_SERVER ['DOCUMENT_ROOT'] . '/sods/lib/session.php';
-
-validar_acesso ();
+	@include $_SERVER['DOCUMENT_ROOT'] . '/sods/includes/topo.php';
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
-<html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		
-		<style type="text/css">
-  			form {
-			    padding-left: 15%;
-			    padding-right: 15%;
-		    }
-  		</style>
-		
-		<title>SODS</title>
-	</head>
-    <body>
-    <?php
-		include '../admin/index.php';
-	?>
-	<!-- formulário para contas de usuário (upd) -->
-		<form role="form">
-		  <div class="col-xs-6">
-		  <div class="form-group">
-		    <label for="nome">Nome</label>
-		    <input type="text" class="form-control" id="nome" value='<?php echo $_SESSION['usuario']['nome_sol'];?>'>
-		  </div>
-		  
-		  <div class="form-group">
-		    <label for="lotacao">Lotação</label>
-		    <input type="text" class="form-control" id="lotacao" value='<?php echo $_SESSION['usuario']['nome_sec'];?>'>
-		  </div>
-		  
-		  <div class="form-group">
-		    <label for="cargo">Cargo</label>
-		    <input type="text" class="form-control" id="cargo" value='<?php echo $_SESSION['usuario']['cargo'];?>'>
-		  </div>
-		  
-		  <div class="form-group">
-		    <label for="telefone_ramal">Telefone/Ramal</label>
-		    <input type="text" class="form-control" id="telefone_ramal" value='<?php echo $_SESSION['usuario']['fone_ramal'];?>'>
-		  </div>
-		  
-		  <div class="form-group">
-		    <label for="email">e-mail</label>
-		    <input type="email" class="form-control" id="email" value='<?php echo $_SESSION['usuario']['email'];?>'>
-		  </div>
-		  
-		  <div class="checkbox-disabled">
-		    <label>
-		      <input type="checkbox" id="tipo_usr" <?php if ($_SESSION['usuario']['tipo_usuario'] == 'A') { echo 'checked' ;}?> disabled="disabled"> Administrador
-		    </label>
-		  
-		  </div>
-		  <button type="submit" class="btn btn-primary pull-right">Atualizar Dados</button>
-		</div>
-		</form>
-	
-    </body>
-</html>
+		<div class="container">
+			<h2 style="text-align: center; margin-bottom: 25px;">Conta</h2>
+			<form role="form" style="width: 50%; margin: 0 auto;>
+				<div class="col-xs-6">
+					<div class="form-group">
+						<label for="nome">Nome</label>
+						<input type="text" id="nome" name="nome" class="form-control" 
+						    value="<?php echo $_SESSION['usuario']['nome_sol'] ?>" />
+					</div>		  
+					<div class="form-group">
+						<label for="lotacao">Lotação</label>
+						<input type="text" id="lotacao" name="lotacao" class="form-control" 
+						    value="<?php echo $_SESSION['usuario']['nome_sec'] ?>" />
+					</div>
+					<div class="form-group">
+						<label for="cargo">Cargo</label>
+						<input type="text" id="cargo" name="cargo" class="form-control" 
+						    value="<?php echo $_SESSION['usuario']['cargo'] ?>" />
+					</div>		  
+					<div class="form-group">
+				    	<label for="telefone_ramal">Telefone / Ramal</label>
+				    	<input type="text" id="telefone_ramal" name="telefone_ramal" class="form-control" 
+				    	    value="<?php echo $_SESSION['usuario']['fone_ramal'] ?>" />
+				  	</div>		  
+					<div class="form-group">
+						<label for="email">E-mail</label>
+						<input type="email" id="email" name="email" class="form-control" 
+						    value="<?php echo $_SESSION['usuario']['email'] ?>" />
+					</div>		  
+					<div class="checkbox-disabled">
+						<label>
+							<input type="checkbox" id="tipo_usr" name="tipo_usr" 
+							    <?php if ($_SESSION['usuario']['tipo_usuario'] == 'A') { echo 'checked="checked"' ;} ?> 
+							    disabled="disabled" /> Administrador
+						</label>		  
+				  	</div>
+					<button type="submit" class="btn btn-primary pull-right">Salvar</button>
+				</div>
+			</form>
+		</div> <!-- container -->
+<?php
+	@include $_SERVER ['DOCUMENT_ROOT'] . '/sods/includes/rodape.php';
+?>
