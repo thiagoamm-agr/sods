27b665ea8020c2bdc35ac7b311d2a95806186446
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-08-18 14:58:51 -0300

    Acrescido @ nos includes da página.

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,9 @@
 <?php
-	include $_SERVER ['DOCUMENT_ROOT'] . '/sods/includes/topo.php';
+	@include $_SERVER ['DOCUMENT_ROOT'] . '/sods/includes/topo.php';
 ?>
-
-<center><img src= "../img/logo_agr.jpg"/></center>
-
+		<center>
+			<img src= "../img/logo_agr.jpg" />
+		</center>
 <?php
-    include $_SERVER ['DOCUMENT_ROOT'] . '/sods/includes/rodape.php';
+    @include $_SERVER ['DOCUMENT_ROOT'] . '/sods/includes/rodape.php';
 ?>
