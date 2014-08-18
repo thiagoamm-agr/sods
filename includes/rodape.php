16754e895af52d647151e637dd0f4a23d83a7609
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-08-18 14:25:23 -0300

    Remoção das tags script. OBS: Agora estão localizadas no topo da página.

--- includes/rodape.php
+++ includes/rodape.php
@@ -1,9 +1,2 @@
-        </div> <!-- container -->
-        
-        <!-- Bootstrap core JavaScript
-        ================================================== -->
-        <!-- Placed at the end of the document so the pages load faster -->
-        <script src="/sods/js/jquery.min.js"></script>
-        <script src="/sods/js/bootstrap/bootstrap.min.js"></script>
     </body>
 </html>
