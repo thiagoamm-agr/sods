965bf918f2cada2eab21918de8aaac639b94f169
edward-agr <edwardneto13@gmail.com>
2014-08-19 17:07:24 -0300

    Criação de arquivo de Model Solicitacao

---
+++ app/models/Solicitacao.php
@@ -0,0 +1,24 @@
+<?php
+
+	class Solicitacao{
+		private $id;
+		private $solicitante_id;
+		private $detalhamento;
+		private $info_adicionais;
+		private $observacoes;
+		private $status;
+		private $observacoes_status;
+		private $data_abertura;
+		private $data_alteracao;
+		private $tipo_solicitacao_id;
+		
+		public function __get($field) {
+			return $this->$field;
+		}
+		
+		public function __set($field, $value) {
+			$this->$field = $value;
+		}
+	}
+
+?>
