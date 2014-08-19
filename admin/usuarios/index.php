bb1505d40b556ad7ba1e62d4d54b9e5092170557
edward-agr <edwardneto13@gmail.com>
2014-08-19 17:10:22 -0300

    Criação de botão 'Adicionar Usuario' e edição de linhas para melhor entendimento do código.

--- admin/usuarios/index.php
+++ admin/usuarios/index.php
@@ -7,6 +7,10 @@
 ?>
 		<div class="container">		
 			<h2>Usuários</h2>
+			<div class="container" >
+					<button class='btn btn-warning btn-sm pull-right' data-toggle='modal' data-target='#modalAdd'><b>Adicionar Usuário</b></button>
+					<br><br>
+			</div>
 			<div class="table-responsive">				
 				<table class="table table-striped table-bordered table-condensed">
 					<thead>
@@ -42,11 +46,11 @@
 							<td><?php echo $usuario['login'] ?></td>
 							<td colspan="2">							
 								<button class='btn btn-primary btn-sm' 
-								    data-toggle='modal' data-target='#modalEdit'>
+								    data-toggle='modal' data-target='#modalEdit' data-book-id="<?php echo $usuario['id']?>">
 									<strong>Editar</strong>
 								</button>
 								<button class='btn btn-danger btn-sm' 
-								    data-toggle='modal' data-target='#modalDel'>
+								    data-toggle='modal' data-target='#modalDel' data-book-id="<?php echo $usuario['id']?>">
 								    <strong>Excluir</strong>
 							    </button>							
 							</td>
@@ -59,6 +63,8 @@
     		</div>
     		    		
 			<!--  Modais -->
+			
+			<!-- Editar Usuário -->
 			<div class="modal fade" id="modalEdit" tabindex="-1" role="dialog" aria-labelledby="modalEdit" 
 			    aria-hidden="true">
 				<div class="modal-dialog">
@@ -120,7 +126,8 @@
 					</div>
 				</div>
 			</div>			
-
+			
+			<!-- Excluir Usuário -->
 			<div class="modal fade .bs-example-modal-sm" id="modalDel" tabindex="-1" role="dialog" 
 			    aria-labelledby="modalDel" aria-hidden="true">
 				<div class="modal-dialog modal-sm">
@@ -142,8 +149,84 @@
     					</form>
     				</div>
     			</div>
-    		</div> <!--  modais -->
+    		</div> 
+    		
+    		<!-- Adicionar Usuário -->
+    		<div class="modal fade" id="modalAdd" tabindex="-1" role="dialog" 
+			    aria-labelledby="modalAdd" aria-hidden="true">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+							    &times;
+							</button>
+							<h4 class="modal-title" id="modalDel">Adicionar Novo Usuário</h4>
+						</div>
+						<div class="modal-body">
+    						<h5>Preencha o Formulário</h5>
+    						 <form role="form" action="#" method="post">
+    							<div class="form-group">
+    								<label for="nome">Nome</label>
+    								<input type="text" class="form-control" name="nome" id="nome"/>    								
+    							</div>
+    							<div class="form-group">
+    								<label for="lotacao">Lotação</label>
+    								<select id="lotacao" name="lotacao" class="form-control">
+<?php 
+										$sql = "select * from secao";
+																
+										$secoes = mysql_query($sql);
+						
+		  								while ($secao = mysql_fetch_assoc($secoes)) {
+?>
+											<option value="<?php echo $secao['id'] ?>">
+												<?php echo $secao['nome']?>
+											</option>
+<?php
+										} 
+?>
+    								</select>
+    							</div>
+    							<div class="form-group">
+    								<label for="cargo">Cargo</label>
+    								<input type="text" class="form-control" name="cargo" id="cargo"/>
+    							</div>
+    							<div class="form-group">
+    								<label for="fone_ramal">Telefone/Ramal</label>
+    								<input type="text" class="form-control" name="fone-ramal" id="fone-ramal"/>
+    							</div>
+    							<div class="form-group">
+    								<label for="email">Email</label>
+    								<input type="text" class="form-control" name="email" id="email"/>
+    							</div>
+    							<div class="form-group">
+    								<label for="login">login</label>
+    								<input type="text" class="form-control" name="login" id="login"/>
+    							</div>
+    							<div class="form-group">
+    								<input type="checkbox" id="tipo_usr" name="tipo_usr" value="A"> Administrador
+    							</div>
+    						</form>
+    					</div>
+						<form action="#">
+	    					<div class="modal-footer">
+	    						<button type="submit" class="btn btn-success">Salvar</button>
+	    						<button type="button" class="btn btn-primary" data-dismiss="modal">Cancelar</button>    						
+	    					</div>
+    					</form>
+    				</div>
+    			</div>
+    		</div>
+    		<!-- Fim  modais -->
+		
 		</div> <!-- container -->
+		
+		<script type="text/javascript">
+			$('#modalDel').on('show.bs.modal', function(e){
+				var bookId = $(e.relatedTarget).data('book-id');
+				$(e.currentTarget).find('input[name="bookId"]').val(bookId);
+			});
+ 		</script>
 <?php
 	@include $_SERVER ['DOCUMENT_ROOT'] . '/sods/includes/rodape.php';
 ?>
