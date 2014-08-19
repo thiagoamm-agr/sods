b55e37e09e6b19fcb486605c1f9301e54fb2d79e
edward-agr <edwardneto13@gmail.com>
2014-08-19 17:08:57 -0300

    Criação de Controller SolicitacoesController

---
+++ app/controllers/SolicitacoesController.php
@@ -0,0 +1,33 @@
+<?php
+	@require $_SERVER['DOCUMENT_ROOT'] . '/sods/app/dao/SolicitacaoDAO.php';
+	
+	class SolicitacoesController {
+		
+		private $dao;
+		
+		public function __construct(){
+			$this->dao = new SolicitacaoDAO();
+		}
+		
+		public function __destruct(){
+			unset($this->dao);
+		}
+		
+		public function __get($field){
+			return $this->$field;
+		}
+		
+		public function __set($field, $value){
+			$this->$field = $value;
+		}
+		
+		public function allAdmin(){
+			return $this->dao->allAdmin();
+		}
+		
+		public function allUser(){
+			return $this->dao->allUser();
+		}
+	}
+
+?>
