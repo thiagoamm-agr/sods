adf7544ed3f13124fb2f9da542e94dfe060c0164
edward-agr <edwardneto13@gmail.com>
2014-08-21 10:22:20 -0300

    Criação de arquivo DAO 'TipoSolicitacao'

---
+++ app/dao/TipoSolicitacaoDAO.php
@@ -0,0 +1,45 @@
+<?php
+	@require $_SERVER['DOCUMENT_ROOT'] . '/sods/app/lib/db.php';
+	
+	@require $_SERVER['DOCUMENT_ROOT'] . '/sods/app/models/TipoSolicitacao.php';
+	
+	class TipoSolicitacaoDAO {
+		
+		private $connection;
+		
+		public function __construct() {
+			$this->connection = get_db_connection();
+		}
+		
+		public function __destruct() {
+			mysql_close($this->connection);
+			unset($connection);
+		}
+		
+		public function __get($field) {
+			return $this->$field;
+		}
+		
+		public function __set($field, $value) {
+			$this->$field = $value;
+		}
+		
+		public function all() {
+			
+		}
+		
+		public function insert() {
+			
+		}
+		
+		public function delete() {
+			
+		}
+		
+		public function update() {
+			
+		}
+		
+	}
+
+?>
