1218b3d5c7e006e020bafdba1c580522c29fc9e4
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-08-21 10:38:25 -0300

    Acrescida linha em branco abaixo do botão Adicionar.

--- admin/solicitacoes/index.php
+++ admin/solicitacoes/index.php
@@ -15,6 +15,9 @@
 					</button>
 				</div>
 			</div>
+			<div class="row">
+				<div class="col-md-12">&nbsp;</div>
+			</div>
 			<div class="table-responsive">
 				<table class="table table-striped table-bordered table-condensed">
 					<thead>
