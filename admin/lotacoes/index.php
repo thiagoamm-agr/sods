93c1ce820110f219d1d87a1ddc9f17933a4b37ea
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-08-22 13:14:21 -0300

    Criação da index do cadastro de lotações.

---
+++ admin/lotacoes/index.php
@@ -0,0 +1,15 @@
+<?php
+	/* Cadastro de Lotações */
+
+	@include $_SERVER['DOCUMENT_ROOT'] . '/sods/includes/topo.php';
+	
+	@include $_SERVER['DOCUMENT_ROOT'] . '/sods/app/models/Lotacao.php';
+	
+	@include $_SERVER['DOCUMENT_ROOT'] . '/sods/app/controllers/LotacoesController.php';
+?>
+		<div class="container">
+			<h2>Lotações</h2>
+		</div>
+<?php
+	@include $_SERVER['DOCUMENT_ROOT'] . '/sods/includes/rodape.php'; 
+?>
