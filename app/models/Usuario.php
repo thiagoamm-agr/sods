dab4e6998ac9281bae44d85566fa6e34ff16d0ed
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-08-22 16:24:05 -0300

    Alterada a estrutura da classe Usuario onde a mesma agora é uma subclasse de Model.

--- app/models/Usuario.php
+++ app/models/Usuario.php
@@ -1,25 +1,18 @@
 <?php
-	class Usuario {
-		
-		private $id;
-		private $nome;
-		private $secao_id;
-		private $cargo;
-		private $fone_ramal;
-		private $email;
-		private $login;
-		private $senha;
-		private $tipo_usuario;
-		private $status;
-		private $data_criacao;
-		private $data_alteracao;
-		
-		public function __get($field) {
-			return $this->$field;
-		}
-		
-		public function __set($field, $value) {
-			$this->$field = $value;
-		}	
+	@require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/models/Model.php';
+	
+	class Usuario extends Model {				
+		protected $id;
+		protected $nome;
+		protected $secao_id;
+		protected $cargo;
+		protected $fone_ramal;
+		protected $email;
+		protected $login;
+		protected $senha;
+		protected $tipo_usuario;
+		protected $status;
+		protected $data_criacao;
+		protected $data_alteracao;
 	}
 ?>
