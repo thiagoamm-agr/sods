1815d1ef29ca271dfdfcccc6833e9cc39c5888fd
edward-agr <edwardneto13@gmail.com>
2014-08-22 14:13:33 -0300

    Atualização de index de solicitações, troca de método de 'while' por 'foreach'

--- admin/solicitacoes/index.php
+++ admin/solicitacoes/index.php
@@ -44,7 +44,7 @@
 					}
 					
 					
-					while ($solicitacao = mysql_fetch_assoc($solicitacoes)) {
+					foreach ($controller->allAdmin() as $solicitacao) {
 
 ?>
 			        	<tr>
@@ -90,6 +90,8 @@
     		</div>
     		
     		<!--  Modais -->
+    		
+    		<!-- Editar Solicitação -->
 			<div class="modal fade" id="modalEdit" tabindex="-1" role="dialog" 
 			    aria-labelledby="modalEdit" aria-hidden="true">
 				<div class="modal-dialog modal-lg">
@@ -109,7 +111,7 @@
 		  						</div>
 		  						<div class="form-group">
 		    						<label for="info_adc" class="col-sm-2 control-label">Inf. Adicionais</label>
-		    						<textarea class="form-control" id="info_adc" name="desc" 
+		    						<textarea class="form-control" id="info_adc" name="info_adc" 
 		    						    rows="4" style="width: 80%;"></textarea>
 		  						</div>
 		  						<div class="form-group">
@@ -129,7 +131,7 @@
 		  										while ($secao = mysql_fetch_assoc($secoes)) {
 ?>
 												<option value="<?php echo $secao['id'] ?>">
-													<?php echo $secao['nome']?>
+															   <?php echo $secao['nome']?>
 												</option>
 <?php
 												}
@@ -147,23 +149,22 @@
 											<label for="ult_alteracao">Ultima Alteração</label>
 											<input type="text" class="form-control" name="ult_alteracao" 
 											    id="ult_alteracao" readonly>
-										</div>
-									
-									
+										</div>									
 									</div>
   								</div>
-							</form>
-						</div>
-							<div class="modal-footer">
-								<button type="button" class="btn btn-primary">Salvar</button>
-								<button type="button" class="btn btn-default" data-dismiss="modal">
+  								<div class="modal-footer">
+									<button type="button" class="btn btn-primary">Salvar</button>
+									<button type="button" class="btn btn-default" data-dismiss="modal">
 								    Fechar
 								</button>
 							</div>
+							</form>
+						</div>
 					</div>
 				</div>
 			</div>			
-
+			
+			<!-- Excluir Solicitação -->
 			<div class="modal fade .bs-example-modal-sm" id="modalDel" tabindex="-1" role="dialog" 
 			    aria-labelledby="modalDel" aria-hidden="true">
 				<div class="modal-dialog modal-sm">
@@ -185,7 +186,111 @@
     					</form>
     				</div>
     			</div>
-    		</div> <!--  modais -->    		
+    		</div> 
+    		
+    		<!-- Adicionar Solicitação -->
+    		<div class="modal fade" id="modalAdd" tabindex="-1" role="dialog" 
+			    aria-labelledby="modalAdd" aria-hidden="true">
+			    <div class="modal-dialog modal-lg">
+			    	<div class="modal-content">
+			    		<div class="modal-header">
+			    			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+			    				&times;
+			    			</button>
+			    			<h4 class="modal-title" id="modalAdd">Adicionar Solicitação</h4>
+			    		</div>
+			    		<div class="modal-body">
+			    			<form id="form-adicionar" role="form" action="#" method="post">
+			    				<div class="form-group">
+				    				<div class="row">
+				    					<div class="col-sm-6">
+				    						<label for="nome">Nome</label>
+		    								<input type="text" class="form-control" name="nome" id="nome"/>
+				    					</div>
+				    					<div class="col-sm-6">
+				    						<label for="lotacao">Lotação</label>
+	    									<select id="lotacao" name="lotacao" class="form-control">
+<?php 
+											$sql = "select * from secao";
+																	
+											$secoes = mysql_query($sql);
+							
+			  								while ($secao = mysql_fetch_assoc($secoes)) {
+?>
+												<option value="<?php echo $secao['id'] ?>">
+															   <?php echo $secao['nome']?>
+												</option>
+<?php
+											} 
+?>
+	    									</select>
+				    					</div>
+	    							</div>
+	    						</div>
+	    						<div class="form-group">
+	    							<label for="desc">Titulo da Solicitação</label>
+	    							<input type="text" class="form-control" name="titulo" id="titulo"/>	    						
+	    						</div>
+	    						<div class="form-group">
+	    							<label for="desc">Descrição do Sistema</label>
+	    							<textarea class="form-control" id="desc" name="desc" 
+		    						    rows="6" style="width: 100%;"></textarea>
+	    						
+	    						</div>
+	    						<div class="form-group">
+	    							<label for="desc">Inf. Adicionais</label>
+	    							<textarea class="form-control" id="info_adc" name=info_adc" 
+		    						    rows="4" style="width: 100%;"></textarea>
+	    						</div>
+	    						
+	    						<div class="form-group">
+	    							<div class="row">
+	    								<div class="col-sm-6">
+	    									<label for="obs">Observações</label>
+	    									<textarea class="form-control" id="obs" name="obs"
+	    										rows="4" style="width: 100%"></textarea>
+	    								</div>
+	    								<div class="col-sm-4">
+	    									<div class="form-group">
+	    										<label for="tp_sol">Tipo de Solicitação</label>
+	    										<select id="tp_sol" name="tp_sol" class="form-control">
+<?php 
+												$sql = "select * from tipo_solicitacao";
+												$secoes = mysql_query($sql);
+																		
+		  										while ($secao = mysql_fetch_assoc($secoes)) {
+?>
+												<option value="<?php echo $secao['id'] ?>">
+															   <?php echo $secao['nome']?>
+												</option>
+<?php
+												}
+?>
+											</select>	    									
+	    									</div>
+	    									<div class="form-group">
+												<label for="data">Data de Criação</label>
+												<input type="text" class="form-control" name="data" 
+											    	id="data" readonly>
+											</div>
+	    								</div>
+	    							</div>
+	    						</div>
+	    						<div class="modal-footer">
+									<button type="button" class="btn btn-primary">Salvar</button>
+									<button type="button" class="btn btn-default" data-dismiss="modal">
+								    	Fechar
+									</button>
+								</div>
+			    			</form>	
+			    		</div>
+			    	</div>
+			    </div>
+    		</div>
+    		
+    		
+    		
+    		<!--  modais -->    		
     	</div> <!-- container -->
 <?php 	
 	@include $_SERVER['DOCUMENT_ROOT'] . '/sods/includes/rodape.php';
