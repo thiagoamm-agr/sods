81f375e541f519bdd9f2b8db1009a21edfcb0c84
edward-agr <edwardneto13@gmail.com>
2014-08-22 14:34:06 -0300

    Correção de links de include para Model e Controller

--- admin/tiposSolicitacao/index.php
+++ admin/tiposSolicitacao/index.php
@@ -3,9 +3,9 @@
 
 @include $_SERVER['DOCUMENT_ROOT'] . '/sods/includes/topo.php';
 
-@include $_SERVER['DOCUMENT_ROOT'] . '/sods/app/models/Lotacao.php';
+@include $_SERVER['DOCUMENT_ROOT'] . '/sods/app/models/TipoSolicitacao.php';
 
-@include $_SERVER['DOCUMENT_ROOT'] . '/sods/app/controllers/LotacoesController.php';
+@include $_SERVER['DOCUMENT_ROOT'] . '/sods/app/controllers/TiposSolicitacaoController.php';
 ?>
 		<div class="container">
 			<h2>Tipos de Solicitação</h2>
