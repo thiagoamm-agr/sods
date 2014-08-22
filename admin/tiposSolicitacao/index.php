5612557966478d0d94a82507780988fc5d480281
edward-agr <edwardneto13@gmail.com>
2014-08-22 14:30:28 -0300

    Index do cadastro de tipos de solicitação

---
+++ admin/tiposSolicitacao/index.php
@@ -0,0 +1,15 @@
+<?php
+/* Cadastro de Tipos de Solicitação */
+
+@include $_SERVER['DOCUMENT_ROOT'] . '/sods/includes/topo.php';
+
+@include $_SERVER['DOCUMENT_ROOT'] . '/sods/app/models/Lotacao.php';
+
+@include $_SERVER['DOCUMENT_ROOT'] . '/sods/app/controllers/LotacoesController.php';
+?>
+		<div class="container">
+			<h2>Tipos de Solicitação</h2>
+		</div>
+<?php
+	@include $_SERVER['DOCUMENT_ROOT'] . '/sods/includes/rodape.php'; 
+?>
