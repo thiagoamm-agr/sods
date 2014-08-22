e8dc4184e749d223ec11a1722e57d01bce9312c7
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-08-22 16:33:37 -0300

    Criada a página do cadastro de solicitações com algum código de teste.

--- admin/lotacoes/index.php
+++ admin/lotacoes/index.php
@@ -10,12 +10,17 @@
 		</div>
 <?php
 	$lotacao = new Lotacao();
-	$lotacao->id = 2;
 	$lotacao->nome = 'Coordenação de Tecnologia da Informação';
 	$lotacao->sigla = 'COTI';
 	$lotacao->gerencia_id = 1;
-	$json = json_encode($lotacao->toArray());
-	var_dump($lotacao->toArray());
-	
+	$json = $lotacao->toJSON();
+	$obj = json_decode($json);
+?>
+		<p><?php echo $json ?></p>
+		<p><?php echo $obj->id ?></p>
+		<p><?php echo $obj->nome ?></p>
+		<p><?php echo $obj->sigla ?></p>
+		<p><?php echo $obj->usuario->nome ?></p>
+<?php	
 	@include $_SERVER['DOCUMENT_ROOT'] . '/sods/includes/rodape.php'; 
 ?>
