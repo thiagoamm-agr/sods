e89fc77af351e8ec264c3976596e5e3bad0fba6d
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-08-22 14:08:46 -0300

    Removida a inclusão de LotacoesController.

--- admin/lotacoes/index.php
+++ admin/lotacoes/index.php
@@ -4,12 +4,18 @@
 	@include $_SERVER['DOCUMENT_ROOT'] . '/sods/includes/topo.php';
 	
 	@include $_SERVER['DOCUMENT_ROOT'] . '/sods/app/models/Lotacao.php';
-	
-	@include $_SERVER['DOCUMENT_ROOT'] . '/sods/app/controllers/LotacoesController.php';
 ?>
 		<div class="container">
 			<h2>Lotações</h2>
 		</div>
 <?php
+	$lotacao = new Lotacao();
+	$lotacao->id = 2;
+	$lotacao->nome = 'Coordenação de Tecnologia da Informação';
+	$lotacao->sigla = 'COTI';
+	$lotacao->gerencia_id = 1;
+	$json = json_encode($lotacao->toArray());
+	var_dump($lotacao->toArray());
+	
 	@include $_SERVER['DOCUMENT_ROOT'] . '/sods/includes/rodape.php'; 
 ?>
