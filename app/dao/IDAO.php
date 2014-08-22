944bbd0728d1018d01960c8adb5e2958dd772773
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-08-22 09:46:12 -0300

    Criação da interface IDAO que estabelece os métodos padrão para todas as classes de objetos DAO.

---
+++ app/dao/IDAO.php
@@ -0,0 +1,11 @@
+<?php
+	interface IDAO {
+		public function insert($obj);
+		public function update($obj);
+		public function save($obj);
+		public function delete($id);
+		public function get($id);
+ 		public function get($field, $value);
+		public function getAll();
+	}
+?>
