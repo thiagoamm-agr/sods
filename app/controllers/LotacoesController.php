32269600d88ba6f68b7377d373b65bafc01df0e2
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-08-26 13:44:25 -0300

    Criação do controlador de lotações.

---
+++ app/controllers/LotacoesController.php
@@ -0,0 +1,36 @@
+<?php
+	@require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/dao/LotacaoDAO.php';
+	
+	class LotacoesController {
+		
+		private $dao;
+		
+		public function __construct() {
+			$this->dao = new LotacaoDAO();
+		}
+		
+		public function __destruct() {
+			unset($this->dao);
+		}
+		
+		public function __get($field) {
+			return $this->$field;
+		}
+		
+		public function __set($field, $value) {
+			$this->$field = $value;
+		}
+		
+		public function insert($lotacao) {
+			$this->dao->insert($lotacao);
+		}
+		
+		public function getLotacoes() {
+			return $this->dao->getAll();
+		}
+		
+		public function getLotacao($id) {
+			return $this->dao->get("id", (int) $id);
+		}
+	}
+?>
