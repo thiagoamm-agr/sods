86ffbc51a6e1055005719b481296609a7327b701
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-08-26 13:42:22 -0300

    Removido o atributo $usuario.

--- app/models/Lotacao.php
+++ app/models/Lotacao.php
@@ -1,15 +1,9 @@
 <?php
 	@require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/models/Model.php';
-	@require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/models/Usuario.php';
 
-	class Lotacao extends Model {		
+	class Lotacao extends Model {	
 		protected $nome;
 		protected $sigla;
-		protected $gerencia_id;
-		protected $usuario;
-		
-		public function __construct() {
-			$this->usuario = new Usuario();
-		}
+		protected $gerencia_id; 
 	}
 ?>
