de13d01ad18ea77b6e20f888781c461607fc5f5c
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-08-29 11:04:45 -0300

    Inclusão de comentários na tag head para melhor identificação dos blocos de código.

--- includes/topo.php
+++ includes/topo.php
@@ -11,24 +11,27 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
     <head>
+    	<!-- Meta -->
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="" />
         <meta name="author" content="" />
  
+ 		<!-- Favicon -->
         <link rel="icon" href="/sods/favicon.ico" />
  
+ 		<!-- CSS -->
 		<link rel="stylesheet" type="text/css" href="/sods/css/bootstrap/bootstrap.css" />
         <link rel="stylesheet" type="text/css" href="/sods/css/bootstrap/bootstrapValidator.css" />
 		<link rel="stylesheet" type="text/css" href="/sods/css/sods.css" />		
 		        
+        <!--  Javascript -->        
         <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     	<!--[if lt IE 9]>
       		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
-
 		<script src="/sods/js/jquery.min.js"></script>        
         <script src="/sods/js/bootstrap/bootstrap.min.js"></script>        
         <script src="/sods/js/bootstrap/bootstrapValidator.js"></script>
