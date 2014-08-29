e8f30ad7661cf7d02746510b908ac2c5040b1685
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-08-29 16:57:17 -0300

    1 - Adição do modificador public em todos os métodos. 2 - Criação do método loadJSON.

--- app/models/Model.php
+++ app/models/Model.php
@@ -24,7 +24,7 @@
 		}
 		
 		// Método que converte o objeto em array.
-		function toArray() {
+		public function toArray() {
 			$argc = func_num_args();
 			if ($argc == 1) {
 				$obj = func_get_arg(0);
@@ -47,8 +47,29 @@
 		/* Método que retorna o objeto em formato
 		 * JSON (Javascript Object Notation).
 		 */
-		function toJSON() {
+		public function toJSON() {
 			return json_encode($this->toArray());
+		}
+		
+		/*
+		 * Método que recebe um JSON (Javascript Object Notation)
+		 * e configura o estado do objeto.
+		 */
+		public function loadJSON($json) {
+			if (isset($json)) {
+				// Converte de JSON para objeto stdClass.
+				$obj = json_decode($json);
+				// Obtém o nome da subclasse.
+				$className = get_class($this);
+				// Cria o objeto para reflexão.
+				$class = new ReflectionClass($className);
+				// Obtém os atributos do objeto. 
+				$args = get_object_vars($obj);
+				// Configura o estado do objeto.
+				foreach ($args as $key => $value) {
+					$this->$key = $value;
+				}
+			}
 		}
 	}
 ?>
