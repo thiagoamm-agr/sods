15496d1b927a44d01552cdfa0dccfb782457ed06
Edward Neto <edwardneto13@gmail.com>
2014-09-09 08:53:37 -0300

    Remoção de icones de validadores.

--- js/validators/tipoSolicitacao.js
+++ js/validators/tipoSolicitacao.js
@@ -5,11 +5,6 @@
 //Modais de Tipo de Solicitação
 function tipo_sol($form) {
 	$($form).bootstrapValidator({
-        feedbackIcons: {
-            valid: 'glyphicon glyphicon-ok',
-            invalid: 'glyphicon glyphicon-remove',
-            validating: 'glyphicon glyphicon-refresh'
-        },
         fields: {
             nome: {
 	            validators: {
