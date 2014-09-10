87ff12a8452403148a57eb3bda6c41dca22fb071
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-09-10 14:20:40 -0300

    Alterada a formatação do código fonte (utilização de 4 espaços para edentação).

--- app/controllers/LotacoesController.php
+++ app/controllers/LotacoesController.php
@@ -1,46 +1,46 @@
 <?php
-	@require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/lib/util.php';
+    @require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/lib/util.php';
 
-	@require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/dao/LotacaoDAO.php';
-	
-	class LotacoesController {
-		
-		private $dao;
-		
-		public function __construct() {
-			$this->dao = new LotacaoDAO();
-		}
-		
-		public function __destruct() {
-			unset($this->dao);
-		}
-		
-		public function __get($field) {
-			return $this->$field;
-		}
-		
-		public function __set($field, $value) {
-			$this->$field = $value;
-		}
-		
-		public function insert($lotacao) {
-			$this->dao->insert($lotacao);
-		}
-		
-		public function getLotacoes() {
-			return $this->dao->getAll();
-		}
-		
-		public function getLotacao($id) {
-			return $this->dao->get("id", (int) $id);
-		}
-		
-		public function getGerencias() {
-			return $this->dao->filter('gerencia_id is null');
-		}
-		
-		public function add($lotacao) {
-			$this->dao->insert($lotacao);
-		}
-	}
+    @require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/dao/LotacaoDAO.php';
+
+    class LotacoesController {
+
+        private $dao;
+
+        public function __construct() {
+            $this->dao = new LotacaoDAO();
+        }
+
+        public function __destruct() {
+            unset($this->dao);
+        }
+
+        public function __get($field) {
+            return $this->$field;
+        }
+
+        public function __set($field, $value) {
+            $this->$field = $value;
+        }
+
+        public function insert($lotacao) {
+            $this->dao->insert($lotacao);
+        }
+
+        public function getLotacoes() {
+            return $this->dao->getAll();
+        }
+
+        public function getLotacao($id) {
+            return $this->dao->get("id", (int) $id);
+        }
+
+        public function getGerencias() {
+            return $this->dao->filter('gerencia_id is null');
+        }
+
+        public function add($lotacao) {
+            $this->dao->insert($lotacao);
+        }
+    }
 ?>
