78265615f4e3d96dc38054049991edd33d443c04
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-09-10 14:42:34 -0300

    Alterada a edentação do código fonte para 4 espaços em branco.

--- app/lib/auth.php
+++ app/lib/auth.php
@@ -7,10 +7,10 @@
         if ($login !== '' && $senha !== '') {
             $conexao = get_db_connection("10.243.1.14", "sods", "dev123");
             $query = "select " . 
-                        "sol.id, sol.nome as nome, sol.lotacao_id, " .
-                        "sol.cargo, sol.telefone, sol.email, sol.login, " . 
-                        "sol.tipo_usuario, sol.status, sol.data_criacao, " . 
-                        "sol.data_alteracao, lot.nome as lotacao, lot.sigla ". 
+                         "sol.id, sol.nome as nome, sol.lotacao_id, " .
+                         "sol.cargo, sol.telefone, sol.email, sol.login, " . 
+                         "sol.tipo_usuario, sol.status, sol.data_criacao, " . 
+                         "sol.data_alteracao, lot.nome as lotacao, lot.sigla ". 
                      "from solicitante as sol " .
                      "inner join lotacao as lot " .
                          "on sol.lotacao_id = lot.id and ".
