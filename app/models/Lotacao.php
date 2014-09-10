fd62e30d7486ff2ba395004b99382cb8072adb3d
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-09-10 14:51:42 -0300

    Alterada a edentação do código fonte para 4 espaços em branco.

--- app/models/Lotacao.php
+++ app/models/Lotacao.php
@@ -1,9 +1,9 @@
 <?php
-	@require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/models/Model.php';
+    @require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/models/Model.php';
 
-	class Lotacao extends Model {	
-		protected $nome;
-		protected $sigla;
-		protected $gerencia_id; 
-	}
+    class Lotacao extends Model {
+        protected $nome;
+        protected $sigla;
+        protected $gerencia_id; 
+    }
 ?>
