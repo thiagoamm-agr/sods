4b349488d06dcad09795692d500ab143b8018e2c
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-09-10 14:22:57 -0300

    Alterada a edentação do código fonte (utilziação de 4 espaços em branco).

--- app/dao/IDAO.php
+++ app/dao/IDAO.php
@@ -1,11 +1,11 @@
 <?php
-	interface IDAO {
-		public function insert($obj);
-		public function update($obj);
-		public function save($obj);
-		public function delete($id);
- 		public function get($field, $value);
-		public function getAll();
-		public function filter($criteria);
-	}
+    interface IDAO {
+        public function insert($obj);
+        public function update($obj);
+        public function save($obj);
+        public function delete($id);
+        public function get($field, $value);
+        public function getAll();
+        public function filter($criteria);
+    }
 ?>
