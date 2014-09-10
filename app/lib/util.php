6b8119b72367298e1720d95d09471ae8975c510e
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-09-10 14:45:15 -0300

    Alterada a edentação do código fonte para 4 espaços em branco.

--- app/lib/util.php
+++ app/lib/util.php
@@ -1,11 +1,11 @@
 <?php
-	// returns true if $str begins with $sub
-	function startsWith($str, $sub) {
-		return (substr($str, 0, strlen($sub)) == $sub);
-	}
+    // returns true if $str begins with $sub
+    function startsWith($str, $sub) {
+        return (substr($str, 0, strlen($sub)) == $sub);
+    }
 
-	// return tru if $str ends with $sub
-	function endsWith($str, $sub) {
-		return (substr($str, strlen($str) - strlen($sub)) == $sub);
-	}
+    // return tru if $str ends with $sub
+    function endsWith($str, $sub) {
+        return (substr($str, strlen($str) - strlen($sub)) == $sub);
+    }
 ?>
