148dcb90a9bb12e15ad794fb4e6a808ff2f095d2
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-09-10 12:04:34 -0300

    Atualização dos links que referenciam o bootstrap.

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap/bootstrap.css" />
+        <link rel="stylesheet" type="text/css" media="screen" href="/sods/bootstrap/css/bootstrap.min.css" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
     	<meta name="viewport" content="width=device-width, initial-scale=1">	
 	    
 	    <!-- Estilo para esse template -->
-	    <link href="css/bootstrap/signin.css" rel="stylesheet" />
+	    <link href="/sods/bootstrap/css/signin.css" rel="stylesheet" />
 	    
 	    <title>Login</title>
     </head>
