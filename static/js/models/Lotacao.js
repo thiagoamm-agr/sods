b94d170c03523da47535f9295321b975a4bc51ea
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-09-10 16:51:55 -0300

    Alteração da edentação do código para 4 espaços em branco.

--- static/js/models/Lotacao.js
+++ static/js/models/Lotacao.js
@@ -1,15 +1,15 @@
 function Lotacao() {
-	this.id = null;
-	this.nome = "";
-	this.sigla = "";
-	this.gerencia_id = null;
+    this.id = null;
+    this.nome = "";
+    this.sigla = "";
+    this.gerencia_id = null;
 }
 
 Lotacao.prototype.toJSON = function() {
-	return '{' +
-		'"id": ' + this.id + ', ' +
-		'"nome": ' + '"' + this.nome + '", ' +
-		'"sigla": "' + this.sigla + '", ' +
-		'"gerencia_id": ' + this.gerencia_id +
-	'}';
+    return '{' +
+        '"id": ' + this.id + ', ' +
+        '"nome": ' + '"' + this.nome + '", ' +
+        '"sigla": "' + this.sigla + '", ' +
+        '"gerencia_id": ' + this.gerencia_id +
+    '}';
 };
