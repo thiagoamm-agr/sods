6d90fb2c081fe1d960d84733dddedfa46fbe5cc1
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-09-10 12:50:10 -0300

    Atualização dos links que referenciam os arquivos estáticos de forma que referenciem /sods/static.

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <link rel="stylesheet" type="text/css" media="screen" href="/sods/bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" media="screen" href="/sods/static/bootstrap/css/bootstrap.min.css" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
     	<meta name="viewport" content="width=device-width, initial-scale=1">	
 	    
 	    <!-- Estilo para esse template -->
-	    <link href="/sods/bootstrap/css/signin.css" rel="stylesheet" />
+	    <link href="/sods/static/bootstrap/css/signin.css" rel="stylesheet" />
 	    
 	    <title>Login</title>
     </head>
