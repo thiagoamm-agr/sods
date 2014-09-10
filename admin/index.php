a04c392e378f48e31a795c62ff597631f0f9055b
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-09-10 12:51:15 -0300

    Atualização dos links que referenciam os arquivos estáticos de forma que referenciem /sods/static.

--- admin/index.php
+++ admin/index.php
@@ -2,7 +2,7 @@
 	@include $_SERVER ['DOCUMENT_ROOT'] . '/sods/includes/topo.php';
 ?>
 		<center>
-			<img src= "/sods/img/logo_agr.jpg" />
+			<img src= "/sods/static/img/logo_agr.jpg" />
 		</center>
 <?php
     @include $_SERVER ['DOCUMENT_ROOT'] . '/sods/includes/rodape.php';
