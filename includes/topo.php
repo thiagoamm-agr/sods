bacc4acee6f780b708cfc1b86b7758b913ae483d
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-09-10 12:50:31 -0300

    Atualização dos links que referenciam os arquivos estáticos de forma que referenciem /sods/static.

--- includes/topo.php
+++ includes/topo.php
@@ -22,9 +22,9 @@
         <link rel="icon" href="/sods/favicon.ico" />
  
  		<!-- CSS -->
-		<link rel="stylesheet" type="text/css" href="/sods/bootstrap/css/bootstrap.min.css" />
-        <link rel="stylesheet" type="text/css" href="/sods/bootstrap/css/bootstrapValidator.min.css" />
-		<link rel="stylesheet" type="text/css" href="/sods/css/sods.css" />		
+		<link rel="stylesheet" type="text/css" href="/sods/static/bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="/sods/static/bootstrap/css/bootstrapValidator.min.css" />
+		<link rel="stylesheet" type="text/css" href="/sods/static/css/sods.css" />
 		        
         <!--  Javascript -->        
         <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -32,9 +32,9 @@
       		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
-		<script src="/sods/js/jquery.min.js"></script>        
-        <script src="/sods/bootstrap/js/bootstrap.min.js"></script>        
-        <script src="/sods/bootstrap/js/bootstrapValidator.min.js"></script>
+		<script src="/sods/static/js/jquery.min.js"></script>        
+        <script src="/sods/static/bootstrap/js/bootstrap.min.js"></script>        
+        <script src="/sods/static/bootstrap/js/bootstrapValidator.min.js"></script>
         
 		<title>SODS</title>
     </head>
