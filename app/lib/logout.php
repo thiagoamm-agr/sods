445feee0cc3f8bb265ceed71fda045e4a7fcabec
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-09-10 14:44:54 -0300

    Alterada a edentação do código fonte para 4 espaços em branco.

--- app/lib/logout.php
+++ app/lib/logout.php
@@ -1,7 +1,7 @@
 <?php
-	session_start();
-	session_unset();
-	session_destroy();
-	
-	header('location: /sods/index.php');
+    session_start();
+    session_unset();
+    session_destroy();
+
+    header('location: /sods/index.php');
 ?>
