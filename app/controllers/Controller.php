4a2b8c51329528e7d783ce7df4ebc06bdf23c6ce
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-09-22 17:15:49 -0300

    Criação da classe abstrata Controller para armazenar todos os atributos e métodos comuns aos controladores da aplicação.

---
+++ app/controllers/Controller.php
@@ -0,0 +1,40 @@
+<?php
+
+    @require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/lib/util.php';
+
+    @require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/lib/paginator.php';
+
+    abstract class Controller {
+
+        protected $dao;
+        protected $paginator; 
+
+        public function __construct() {
+            $this->paginator = new Paginator();
+        }
+
+        public function __destruct() {
+            unset($this->paginator);
+        }
+
+        // Getters
+        public function __get($field) {
+            return $this->$field;
+        }
+
+        // Setters
+        public function __set($field, $value) {
+            $this->$field = $value;
+        }
+
+        // abstract protected function getAll();
+        
+        abstract public function add($model);
+
+        abstract public function edit($model);
+
+        abstract public function del($id);
+
+        abstract public function paginate();
+    }
+?>
