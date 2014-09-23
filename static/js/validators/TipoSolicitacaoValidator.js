f9f321559a6fb599cf41a8428612436afdf6d0e0
Edward Neto <edwardneto13@gmail.com>
2014-09-23 16:40:18 -0300

    Atualização de classe validator do Tipo de Usuario.

--- static/js/validators/TipoSolicitacaoValidator.js
+++ static/js/validators/TipoSolicitacaoValidator.js
@@ -2,24 +2,46 @@
  * 
  */
 
-function TipoSolicitacaoValidator() {}
+/**
+ * Validador dos formulários de adição e edição de Tipo de Solicitacao.
+ */
 
-TipoSolicitacaoValidator.validate = function(form){
-	form.bootstrapValidator({
-		 live: "enabled",
-		 feedbackIcons: {
-		        valid: 'glyphicon glyphicon-ok',
-		    invalid: 'glyphicon glyphicon-remove',
-		    validating: 'glyphicon glyphicon-refresh'
-		},
-		fields: {
-			nome: {
-				validators: {
-					notEmpty: {
-		            	message: 'Este campo deve ser preenchido'
-		            }
-	            }
-			}
-		}
-	});
+function TipoSolicitacaoValidator(form) {
+    this.form = form;
+    this.data = null;
+    if (form != null) {
+        form.bootstrapValidator({
+            live: 'enabled',
+            feedbackIcons: {
+                valid: 'glyphicon glyphicon-ok',
+                invalid: 'glyphicon glyphicon-remove',
+                validating: 'glyphicon glyphicon-refresh'
+            },
+            fields: {
+                nome: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Nome é um campo de preenchimento obrigatório.'
+                        }
+                    }
+                }
+            }
+        });
+        this.data = this.form.data('bootstrapValidator');
+    }
 }
+
+TipoSolicitacaoValidator.prototype.validate = function() {
+    var valid = false;
+    if (this.form != null) {
+        this.data.validate();
+        valid = this.data.isValid();
+    }
+    return valid;
+}
+
+TipoSolicitacaoValidator.prototype.resetForm = function() {
+    if (this.data != null) {
+        this.data.resetForm(true);
+    }
+};
