965d084e48d74604df1ce51954cf022853307815
Edward Neto <edwardneto13@gmail.com>
2014-09-23 16:39:00 -0300

    atualização de consulta sql.

--- app/dao/TipoSolicitacaoDAO.php
+++ app/dao/TipoSolicitacaoDAO.php
@@ -92,6 +92,7 @@
 				if (!empty($columns) && !empty($values)) {
 					try {
 						$query = "update tipo_solicitacao set status = 'I' where id = $values";
+						//$query = "delete from tipo_solicitacao where id = {$tipoSolicitacao->id}";
 						mysql_query($query, $this->connection);
 					} catch (Exception $e) {
 						echo $e;
@@ -100,41 +101,6 @@
 			}
 			return;
 		}
-		
-/*		public function update($tipoSolicitacao) {
-			if (isset($tipoSolicitacao)){
-				$class = new ReflectionClass('TipoSolicitacao');
-				$properties = $class->getProperties();
-				$columns = "";
-				$values = "";
-				foreach ($properties as $property) {
-					$property->setAccessible(true);
-					$column = $property->getName();
-					$value = $property->getValue($tipoSolicitacao);
-					if ($column != 'id') {
-						$columns .= "{$column}, ";
-					}
-					if (!empty($value)) {
-						if($column != 'id'){
-							$values .= "'{$value}', ";
-						}else{
-							$id = "{$value}";
-						}
-					}
-				}
-				$columns = substr($columns, 0, strrpos($columns, ", "));
-				$values = substr($values, 0, strrpos($values, ", "));
-				if (!empty($columns) && !empty($values)) {
-					try {
-						$query = "update tipo_solicitacao set $columns = $values where id = $id";
-						mysql_query($query, $this->connection);
-					} catch (Exception $e) {
-						echo $e;
-					}					
-				}
-			}
-			return;		
-		}*/
 		
 		public function update($tipoSolicitacao) {
 			if (isset($tipoSolicitacao)) {
