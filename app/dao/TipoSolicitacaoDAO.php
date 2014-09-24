9af188720ee32f00110abe3b6447c1e308622c6f
Edward Neto <edwardneto13@gmail.com>
2014-09-24 15:27:30 -0300

    Adequação da classe para implementar a classe IDAO.

--- app/dao/TipoSolicitacaoDAO.php
+++ app/dao/TipoSolicitacaoDAO.php
@@ -3,7 +3,7 @@
 	
 	@require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/models/TipoSolicitacao.php';
 	
-	class TipoSolicitacaoDAO {
+	class TipoSolicitacaoDAO implements IDAO {
 		
 		private $connection;
 		
@@ -22,18 +22,6 @@
 		
 		public function __set($field, $value) {
 			$this->$field = $value;
-		}
-		
-		public function getAll() {
-			$query = "select * from tipo_solicitacao order by id desc";
-		
-			$result = mysql_query($query, $this->connection);
-			$all = array();
-			while ($row = mysql_fetch_assoc($result)){
-				array_push($all, $row);
-			}
-		
-			return $all;
 		}
 		
 		public function insert($tipoSolicitacao) {
@@ -70,38 +58,6 @@
 			return;			
 		}
 		
-		public function delete($tipoSolicitacao) {
-			if (isset($tipoSolicitacao)){
-				$class = new ReflectionClass('TipoSolicitacao');
-				$properties = $class->getProperties();
-				$columns = "";
-				$values = "";
-				foreach ($properties as $property) {
-					$property->setAccessible(true);
-					$column = $property->getName();
-					$value = $property->getValue($tipoSolicitacao);					
-					if ($column == 'id') {
-						$columns .= "{$column}, ";
-						if(!empty($value)){
-							$values .= (int) $value;
-						}
-					}
-				}
-				$columns = substr($columns, 0, strrpos($columns, ", "));
-				//$values = substr($values, 0, strrpos($values, ", "));
-				if (!empty($columns) && !empty($values)) {
-					try {
-						$query = "update tipo_solicitacao set status = 'I' where id = $values";
-						//$query = "delete from tipo_solicitacao where id = {$tipoSolicitacao->id}";
-						mysql_query($query, $this->connection);
-					} catch (Exception $e) {
-						echo $e;
-					}
-				}
-			}
-			return;
-		}
-		
 		public function update($tipoSolicitacao) {
 			if (isset($tipoSolicitacao)) {
 				$class = new ReflectionClass('TipoSolicitacao');
@@ -133,6 +89,70 @@
 				}
 			}
 		}
+		
+		public function save($tipoSolicitacao) {
+			if (isset($tipoSolicitacao)){
+		
+			}
+		}
+		
+		public function delete($tipoSolicitacao) {
+			if (isset($tipoSolicitacao)){
+				$class = new ReflectionClass('TipoSolicitacao');
+				$properties = $class->getProperties();
+				$columns = "";
+				$values = "";
+				foreach ($properties as $property) {
+					$property->setAccessible(true);
+					$column = $property->getName();
+					$value = $property->getValue($tipoSolicitacao);					
+					if ($column == 'id') {
+						$columns .= "{$column}, ";
+						if(!empty($value)){
+							$values .= (int) $value;
+						}
+					}
+				}
+				$columns = substr($columns, 0, strrpos($columns, ", "));
+				//$values = substr($values, 0, strrpos($values, ", "));
+				if (!empty($columns) && !empty($values)) {
+					try {
+						//$query = "update tipo_solicitacao set status = 'I' where id = $values";
+						$query = "delete from tipo_solicitacao where id = {$tipoSolicitacao->id}";
+						mysql_query($query, $this->connection);
+					} catch (Exception $e) {
+						echo $e;
+					}
+				}
+			}
+			return;
+		}
+		
+		public function get($field, $value) {
+			if (isset($field) && isset($value)) {
+				
+			}
+		}
+		
+		public function getAll() {
+			$query = "select * from tipo_solicitacao order by id desc";
+		
+			$result = mysql_query($query, $this->connection);
+			$all = array();
+			while ($row = mysql_fetch_assoc($result)){
+				array_push($all, $row);
+			}
+		
+			return $all;
+		}
+		
+		public function filter($criteria) {
+			if (isset($criteria)) {
+				
+			}
+		}
+		
+
 		
 	}
 
