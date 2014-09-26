87bf0ad754af102b1c0a380acc53b42f929773b4
Edward Neto <edwardneto13@gmail.com>
2014-09-26 10:42:50 -0300

    Correção de classe para atender a novo padrão que extende a classe abstrata Controller.

--- app/controllers/TiposSolicitacoesController.php
+++ app/controllers/TiposSolicitacoesController.php
@@ -1,16 +1,21 @@
 <?php
+	
+	@require_once 'Controller.php';
+	
 	@require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/dao/TipoSolicitacaoDAO.php';
 	
-	class TiposSolicitacoesController {
-		
-		private $dao;
+	class TiposSolicitacoesController extends Controller {
 		
 		public function __construct() {
+			//faz a chamada do construtor da superclasse
+			parent::__construct();
 			$this->dao = new TipoSolicitacaoDAO();
 		}
 		
 		public function __destruct() {
 			unset($this->dao);
+			//faz a chamada do destrutor da superclasse
+			parent::__destruct();
 		}
 		
 		public function __get($field) {
@@ -21,21 +26,35 @@
 			$this->$field = $value;
 		}
 		
-		public function getTipos(){
+		public function _list(){
 			return $this->dao->getAll();
 		}
 		
-		public function insert($tipoSolicitacao) {
+		public function add($tipoSolicitacao) {
 			return $this->dao->insert($tipoSolicitacao);			
+		}
+		
+		public function edit($tipoSolicitacao) {
+			return $this->dao->update($tipoSolicitacao);
 		}
 		
 		public function delete($tipoSolicitacao) {
 			return $this->dao->delete($tipoSolicitacao);
 		}
 		
-		public function update($tipoSolicitacao) {
-			return $this->dao->update($tipoSolicitacao);
+		public function paginate() {
+			$this->paginator->pagenumber = 1;
+			$this->paginator->pagesize = 1;
+			$this->paginator->totalrecords = 4;
+			$this->paginator->showfirst = true;
+			$this->paginator->showlast = true;
+			$this->paginator->paginationcss = "pagination-normal";
+			$this->paginator->paginationstyle = 1; // 1: advance, 0: normal
+			$this->paginator->defaultUrl = "/sods/admin/lotacoes/index.php";
+			$this->paginator->paginationUrl = "/sods/admin/lotacoes/index.php?p=[p]";
+			return $this->paginator->paginate();
 		}
+		
 	}
 
 ?>
