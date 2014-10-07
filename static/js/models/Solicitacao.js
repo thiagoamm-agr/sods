d1218e035bccb589e5e1803f6cb3790428f0eb10
Edward Neto <edwardneto13@gmail.com>
2014-10-07 14:51:03 -0300

    Adequação dos nomes das variáveis para padrão do BD.

--- static/js/models/Solicitacao.js
+++ static/js/models/Solicitacao.js
@@ -1,25 +1,25 @@
 function Solicitacao() {
     this.id = null;
-    this.solicitanteId = null;
+    this.solicitante_id = null;
     this.titulo = "";
     this.detalhamento = "";
-    this.infoAdicionais = "";
+    this.info_adicionais = "";
     this.observacoes = "";
     this.status = "";
-    this.observacoesStatus = "";
-    this.tipoSolicitacaoId = null;
+    this.observacoes_status = "";
+    this.tipo_solicitacao_id = null;
 }
 
 Solicitacao.prototype.toJSON = function() {
     return '{' +
         '"id": ' + this.id + ', ' +
-        '"solicitante_id": ' + this.solicitanteId + ', ' +
+        '"solicitante_id": ' + this.solicitante_id + ', ' +
         '"titulo": ' + '"' + this.titulo + '", ' +
         '"detalhamento": ' + '"' + this.detalhamento + '", ' +
-        '"info_adicionais": ' + '"' + this.infoAdicionais + '", ' +
+        '"info_adicionais": ' + '"' + this.info_adicionais + '", ' +
         '"observacoes": ' + '"' + this.observacoes + '", ' +
         '"status": ' + '"' + this.status + '", ' +
-        '"observacoes_status": ' + '"' + this.observacoesStatus + '", ' +
-        '"tipo_solicitacao_id": ' + this.tipoSolicitacaoId +
+        '"observacoes_status": ' + '"' + this.observacoes_status + '", ' +
+        '"tipo_solicitacao_id": ' + this.tipo_solicitacao_id +
     '}';
 };
