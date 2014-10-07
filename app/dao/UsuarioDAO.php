7b41542ea55cfb430668cd1be922c2f467ba342a
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-10-07 14:07:36 -0300

    Alteração da interface IDAO para DAO.

--- app/dao/UsuarioDAO.php
+++ app/dao/UsuarioDAO.php
@@ -1,68 +1,68 @@
 <?php
-	@require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/lib/db.php';
+    @require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/lib/db.php';
 
-	@require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/dao/IDAO.php';
-	
-	@require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/models/Usuario.php';
-	
-	class UsuarioDAO implements IDAO{
-		
-		private $connection;
-		
-		public function __construct() {
-			$this->connection = get_db_connection();
-		}
-		
-		public function __destruct() {
-			mysql_close($this->connection);
-			unset($this->connection);
-		}
-		
-		public function __get($field) {
-			return $this->$field;
-		}
-		
-		public function __set($field, $value) {
-			$this->$field = $value;
-		}		
-		
-		public function insert($usuario) {
-			if (isset($usuario)) {
-				$class = new ReflectionClass('Usuario');
-				$properties = $class->getProperties();
-				$columns = "";
-				$values = "";
-				foreach ($properties as $property) {
-					$property->setAccessible(true);
-					$column = $property->getName();
-					$value = $property->getValue($usuario);
-					if (($column != 'id') && 
-						($column != 'status') && 
-						($column != 'data_criacao') && 
-						($column != 'data_alteracao')) {
-							$columns .= "{$column}, ";
-						if (gettype($value) == "string"){
-							$values .= "'{$value}', ";
-						} else {
-							if (endsWith($value, '_id')){
-								$value = (int) $value;
-							}
-							$values .= "{$value}, ";
-						}
-					}
-				}
-				$columns = substr($columns, 0, strrpos($columns, ", "));
-				$values = substr($values, 0, strrpos($values, ", "));
-				if(!empty($columns) && !empty($values)){
-					$query = "insert into solicitante ($columns) values ($values)";
-					mysql_query($query, $this->connection);
-				}
-			}
-			return;
-		}
-		
-		public function update($usuario) {
-			if (isset($usuario)) {
+    @require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/dao/DAO.php';
+    
+    @require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/models/Usuario.php';
+    
+    class UsuarioDAO implements DAO{
+        
+        private $connection;
+        
+        public function __construct() {
+            $this->connection = get_db_connection();
+        }
+        
+        public function __destruct() {
+            mysql_close($this->connection);
+            unset($this->connection);
+        }
+        
+        public function __get($field) {
+            return $this->$field;
+        }
+        
+        public function __set($field, $value) {
+            $this->$field = $value;
+        }        
+        
+        public function insert($usuario) {
+            if (isset($usuario)) {
+                $class = new ReflectionClass('Usuario');
+                $properties = $class->getProperties();
+                $columns = "";
+                $values = "";
+                foreach ($properties as $property) {
+                    $property->setAccessible(true);
+                    $column = $property->getName();
+                    $value = $property->getValue($usuario);
+                    if (($column != 'id') && 
+                        ($column != 'status') && 
+                        ($column != 'data_criacao') && 
+                        ($column != 'data_alteracao')) {
+                            $columns .= "{$column}, ";
+                        if (gettype($value) == "string"){
+                            $values .= "'{$value}', ";
+                        } else {
+                            if (endsWith($value, '_id')){
+                                $value = (int) $value;
+                            }
+                            $values .= "{$value}, ";
+                        }
+                    }
+                }
+                $columns = substr($columns, 0, strrpos($columns, ", "));
+                $values = substr($values, 0, strrpos($values, ", "));
+                if(!empty($columns) && !empty($values)){
+                    $query = "insert into solicitante ($columns) values ($values)";
+                    mysql_query($query, $this->connection);
+                }
+            }
+            return;
+        }
+        
+        public function update($usuario) {
+            if (isset($usuario)) {
                 $class = new ReflectionClass('Usuario');
                 $properties = $class->getProperties();
                 $columns = "";
@@ -91,84 +91,88 @@
                     mysql_query($query, $this->connection);
                 }
             }
-		}
-		
-		public function save($usuario) {
-			if (isset($usuario)) {
-				if (isset($usuario->id)) {
-		
-				}
-			}
-		}
-		
-		public function delete($usuario) {
-			if (isset($usuario)){
-				$class = new ReflectionClass('Usuario');
-				$properties = $class->getProperties();
-				$columns = "";
-				$values = "";
-				foreach ($properties as $property) {
-					$property->setAccessible(true);
-					$column = $property->getName();
-					$value = $property->getValue($usuario);					
-					if ($column == 'id') {
-						$columns .= "{$column}, ";
-						if(!empty($value)){
-							$values .= (int) $value;
-						}
-					}
-				}
-				$columns = substr($columns, 0, strrpos($columns, ", "));
-				if (!empty($columns) && !empty($values)) {
-					try {
-						$query = "update solicitante set status = 'I' where id = $values";
-						//$query = "delete from usuario where id = $values";
-						mysql_query($query, $this->connection);
-					} catch (Exception $e) {
-						echo $e;
-					}
-				}
-			}
-			return;
-		}
-		
-		public function get($field, $value) {
-			if (isset($field) && isset($value)) {
-				if (gettype($value) == 'string') {
-					$value = "'$value'";
-				} 
-				$query = "select * from solicitante where $field = $value";
-				$result = mysql_query($query, $this->connection);
-				return mysql_fetch_object($result);
-			}
-		}
-		
-		public function getAll() {
-			$query = "select " .
-					     "s.id, s.nome as nome_sol, l.id as lotacao_id, l.nome as lotacao, s.cargo, " .
-					     "s.telefone, s.login, s.tipo_usuario, s.status, s.email " .
-					 "from " .
-					     "solicitante as s " .
-					 "inner join lotacao as l " .
-					     "on s.lotacao_id = l.id order by s.id desc";			
-			$result = mysql_query($query, $this->connection);
-			$all = array();
-			while ($row = mysql_fetch_assoc($result)) {
-				array_push($all, $row);
-			}
-			return $all;
-		}
-		
-		public function filter($criteria) {
-			if (isset($criteria)) {
-				
-			}
-		}
-		
-		public function count($criteria){
-			if (isset($criteria)){
-		
-			}
-		}
-	}
+        }
+        
+        public function save($usuario) {
+            if (isset($usuario)) {
+                if (isset($usuario->id)) {
+        
+                }
+            }
+        }
+        
+        public function delete($usuario) {
+            if (isset($usuario)){
+                $class = new ReflectionClass('Usuario');
+                $properties = $class->getProperties();
+                $columns = "";
+                $values = "";
+                foreach ($properties as $property) {
+                    $property->setAccessible(true);
+                    $column = $property->getName();
+                    $value = $property->getValue($usuario);                    
+                    if ($column == 'id') {
+                        $columns .= "{$column}, ";
+                        if(!empty($value)){
+                            $values .= (int) $value;
+                        }
+                    }
+                }
+                $columns = substr($columns, 0, strrpos($columns, ", "));
+                if (!empty($columns) && !empty($values)) {
+                    try {
+                        $query = "update solicitante set status = 'I' where id = $values";
+                        //$query = "delete from usuario where id = $values";
+                        mysql_query($query, $this->connection);
+                    } catch (Exception $e) {
+                        echo $e;
+                    }
+                }
+            }
+            return;
+        }
+        
+        public function get($field, $value) {
+            if (isset($field) && isset($value)) {
+                if (gettype($value) == 'string') {
+                    $value = "'$value'";
+                } 
+                $query = "select * from solicitante where $field = $value";
+                $result = mysql_query($query, $this->connection);
+                return mysql_fetch_object($result);
+            }
+        }
+        
+        public function getAll() {
+            $query = "select " .
+                         "s.id, s.nome as nome_sol, l.id as lotacao_id, l.nome as lotacao, s.cargo, " .
+                         "s.telefone, s.login, s.tipo_usuario, s.status, s.email " .
+                     "from " .
+                         "solicitante as s " .
+                     "inner join lotacao as l " .
+                         "on s.lotacao_id = l.id order by s.id desc";
+            $result = mysql_query($query, $this->connection);
+            $all = array();
+            while ($row = mysql_fetch_assoc($result)) {
+                array_push($all, $row);
+            }
+            return $all;
+        }
+        
+        public function filter($criteria) {
+            if (isset($criteria)) {
+                
+            }
+        }
+        
+        public function count($criteria){
+            if (isset($criteria)){
+        
+            }
+        }
+        
+        public function rowSet($size=10, $start=0) {
+            return null;
+        }
+    }
 ?>
