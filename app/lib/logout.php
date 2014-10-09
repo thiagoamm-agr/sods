a2c9a7d89bf6fca6f5753e164456210a10c38517
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-10-09 10:07:21 -0300

    Chamada a função script_guard no início do script para restringir o acesso direto (via barra de endereço do navegador) por parte do usuário.

--- app/lib/logout.php
+++ app/lib/logout.php
@@ -1,4 +1,10 @@
 <?php
+
+    @require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/lib/util.php';
+
+    // Protege o script de acesso direto.
+    script_guard();
+
     session_start();
     session_unset();
     session_destroy();
