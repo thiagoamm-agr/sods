81cef239fe89593d75e2b534cf2ca29f3b3320cf
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-10-09 10:12:53 -0300

    Aplicada a formatação de 4 espaços em branco.

--- index.php
+++ index.php
@@ -7,37 +7,37 @@
         <meta charset="utf-8" />
         <link rel="stylesheet" type="text/css" media="screen" href="/sods/static/bootstrap/css/bootstrap.min.css" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    	<meta name="viewport" content="width=device-width, initial-scale=1">	
-	    
-	    <!-- Estilo para esse template -->
-	    <link href="/sods/static/bootstrap/css/signin.css" rel="stylesheet" />
-	    
-	    <title>Login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- Estilo para esse template -->
+        <link href="/sods/static/bootstrap/css/signin.css" rel="stylesheet" />
+        
+        <title>Login</title>
     </head>
-    	<body>
-        	<center><h2><b>SODS</b> - Sistema de Solicitação de Desenvolvimento de Software</h2></center>
-        		<div class="container">
-			      <form class="form-signin" role="form" method="post" id="formlogin" name="formlogin">
-			        <h3 class="form-signin-heading">Login</h3>
-			        <input type="login" id="login" name="login" class="form-control" placeholder="Login" 
-			            required autofocus />
-			        <input type="password" id="senha" name="senha" class="form-control" placeholder="Senha" 
-			            required />
-			        <div class="checkbox">
-			          <label>
-			            <input type="checkbox" value="remember-me"> Lembre-me
-			          </label>
-			        </div>
-			        <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
-			      </form>
-    			</div> 
+        <body>
+            <center><h2><b>SODS</b> - Sistema de Solicitação de Desenvolvimento de Software</h2></center>
+                <div class="container">
+                  <form class="form-signin" role="form" method="post" id="formlogin" name="formlogin">
+                    <h3 class="form-signin-heading">Login</h3>
+                    <input type="login" id="login" name="login" class="form-control" placeholder="Login" 
+                        required autofocus />
+                    <input type="password" id="senha" name="senha" class="form-control" placeholder="Senha" 
+                        required />
+                    <div class="checkbox">
+                      <label>
+                        <input type="checkbox" value="remember-me"> Lembre-me
+                      </label>
+                    </div>
+                    <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+                  </form>
+                </div> 
 <?php
-    @require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/lib/auth.php';     
+    @require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/lib/auth.php';
             
     @$login = $_POST['login'];
     @$senha = $_POST['senha'];
 
-    if (autenticar_usuario($login, $senha)) {        
+    if (autenticar_usuario($login, $senha)) {
         header('location: /sods/admin');
     }
 ?>
