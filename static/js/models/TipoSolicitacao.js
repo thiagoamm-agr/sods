5b06028eeefcac27b3d38680e2359631d10242e8
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-10-13 14:55:17 -0300

    Adição do nome do arquivo no comentário do cabeçalho.

--- static/js/models/TipoSolicitacao.js
+++ static/js/models/TipoSolicitacao.js
@@ -1,17 +1,17 @@
 /**
- * 
+ * TipoSolicitacao.js 
  */
 
 function TipoSolicitacao() {
-	this.id = null;
-	this.nome = "";
-	this.status = "";
+    this.id = null;
+    this.nome = "";
+    this.status = "";
 }
 
 TipoSolicitacao.prototype.toJSON = function() {
-	return '{' +
-		'"id": ' + this.id + ', ' +
-		'"nome": "' + this.nome + '" ,' +
-		'"status":"'  + this.status + '"' +
-	'}';
+    return '{' +
+        '"id": ' + this.id + ', ' +
+        '"nome": "' + this.nome + '" ,' +
+        '"status": "' + this.status + '"' +
+    '}';
 };
