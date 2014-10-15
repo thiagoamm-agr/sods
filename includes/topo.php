af3847bc7c94941129cfe3349f217d55a76e75b9
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-10-15 17:28:21 -0300

    Atualizada a formatação do código-fonte.

--- includes/topo.php
+++ includes/topo.php
@@ -26,14 +26,14 @@
         <link rel="stylesheet" type="text/css" href="/sods/static/css/sods.css" />
         <link rel="stylesheet" type="text/css" href="/sods/static/css/theme.default.css" />
                 
-        <!--  Javascript -->        
+        <!--  Javascript -->
         <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!--[if lt IE 9]>
               <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
               <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
-        <script src="/sods/static/js/jquery.min.js"></script>        
-        <script src="/sods/static/bootstrap/js/bootstrap.min.js"></script>        
+        <script src="/sods/static/js/jquery.min.js"></script>
+        <script src="/sods/static/bootstrap/js/bootstrap.min.js"></script>
         <script src="/sods/static/bootstrap/js/bootstrapValidator.min.js"></script>
         <script src="/sods/static/js/jquery.tablesorter.min.js"></script>
         <script src="/sods/static/js/jquery.tablesorter.widgets.min.js"></script>
@@ -45,12 +45,12 @@
                 $(function(){
                     $("#tablesorter").tablesorter(
                         {
-                            emptyTo: 'none',    
+                            emptyTo: 'none',
                             theme : 'default',
                             headerTemplate : '{content}{icon}',
                             widgetOptions : {
                               columns : [ "primary", "secondary", "tertiary" ],
-                        }        
+                        }
                     });
                 });
             });
