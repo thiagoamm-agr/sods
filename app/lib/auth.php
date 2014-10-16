6fa8780b81cce5f0ff86a70e64381f5164c13880
Edward Neto <edwardneto13@gmail.com>
2014-10-16 11:02:17 -0300

    Modificação da query para retornar somente usuários que estejam ativos;

--- app/lib/auth.php
+++ app/lib/auth.php
@@ -7,7 +7,7 @@
         $senha = isset($senha) ? trim($senha) : '';
         if ($login !== '' && $senha !== '') {
             $conexao = get_db_connection("10.243.1.14", "sods", "dev123");
-            $query = "select * from solicitante where login = '$login' and senha = md5('$senha')";
+            $query = "select * from solicitante where login = '$login' and senha = md5('$senha') and status <> 'I'";
             $result = mysql_query($query);
             if (mysql_num_rows($result) > 0) {
                 $_SESSION['usuario'] = mysql_fetch_array($result);
