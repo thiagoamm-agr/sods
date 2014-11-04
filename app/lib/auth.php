ba479319a9bce9b775ac809dc536b62c3a04ab30
Edward Neto <edwardneto13@gmail.com>
2014-11-04 11:27:50 -0200

    Retirada de linha de código comentada não utilizada (query antiga)

--- app/lib/auth.php
+++ app/lib/auth.php
@@ -7,11 +7,10 @@
         $senha = isset($senha) ? trim($senha) : '';
         if ($login !== '' && $senha !== '') {
             $conexao = get_db_connection("10.243.1.14", "sods", "dev123");
-            //$query = "select * from solicitante where login = '$login' and senha = md5('$senha') and status <> 'I'";
-            $query = "select s.id, s.nome, l.nome as nome_lotacao, l.id as id_lotacao, s.cargo, s.telefone, s.email, s.login, s.senha, 
-            		s.tipo_usuario,	s.status, s.data_criacao, s.data_alteracao from solicitante as s 
-            		inner join lotacao as l on s.lotacao_id = l.id and 
-            		s.login = '$login' and s.senha = md5('$senha') and s.status <> 'I';";
+            $query = "select s.id, s.nome, l.nome as nome_lotacao, l.id as id_lotacao, s.cargo, s.telefone, s.email, 
+                      s.login, s.senha, s.tipo_usuario,	s.status, s.data_criacao, s.data_alteracao 
+                      from solicitante as s inner join lotacao as l on s.lotacao_id = l.id and 
+            		  s.login = '$login' and s.senha = md5('$senha') and s.status <> 'I';";
             $result = mysql_query($query);
             if (mysql_num_rows($result) > 0) {
                 $_SESSION['usuario'] = mysql_fetch_array($result);
