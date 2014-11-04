b839ec6cf3169cc6b5295a01f6f4bb3a1d5da923
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-11-04 18:28:11 -0200

    1 - Alterado o nome do método getRows para getPage. 2 - Alterados os nomes dos parâmetros do método getRows. 3 - Alteradas as ocorrências do método rowSet para paginate.

--- app/controllers/UsuariosController.php
+++ app/controllers/UsuariosController.php
@@ -2,11 +2,11 @@
     @require_once 'Controller.php';
 
     @require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/dao/UsuarioDAO.php';
-    
+
     @include_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/controllers/SolicitacoesController.php';
 
     class UsuariosController extends Controller {
-        
+
         public function __construct() {
             //faz a chamada do construtor da superclasse
             parent::__construct();
@@ -41,79 +41,78 @@
         public function count($criteria = null) {
             return $this->dao->count($criteria);
         }
-        
+
         public function getUsuario($field, $value) {
             return $this->dao->get($field, $value);
         }
 
-        public function getRows($page=1, $size=10) {
-            if (empty($page)) {
-                $page = 1;
+        public function getPage($page_number=1, $rows=10) {
+            if (empty($page_number)) {
+                $page_number = 1;
             }
-            $this->paginator->pagesize = $size;
-            $this->paginator->pagenumber = $page;
-            return $this->dao->rowSet($size, $page * $size - $size);
+            $this->paginator->pagesize = $rows;
+            $this->paginator->pagenumber = $page_number;
+            return $this->dao->paginate($rows, $page_number * $rows - $rows);
         }
-        
+
         public function getForm($id) {
-        	$usuario = $this->getUsuario('s.id', $id);
-        	
-        	$html = "<div><h2>Conta</h2></div>";
-        	$html .= "<div class=\"form-group\">";
-        	$html .= "<label for=\"nome\">Nome</label>";
-        	$html .= "<input type=\"text\" id=\"nome\" name=\"nome\" class=\"form-control\"";
-        	$html .= "value=\"{$usuario['nome']}\"/>";
-        	$html .= "</div>";
-        	$html .= "<div class=\"form-group\">";
-        	$html .= "<label for=\"lotacao\">Lotação</label>";
-        	$html .= "<select id=\"lotacao_id\" name=\"lotacao_id\" class=\"form-control\">";
-        	$lotacoesController = new LotacoesController();
-        	$lotacoes = $lotacoesController->_list();
-        	foreach ($lotacoes as $lotacao) {
-        		$html .= "<option value=\"{$lotacao['id']}\"> {$lotacao['nome']} </option>";
-        	}
-        	$html .= "</select></div>";
-        	$html .= "<div class=\"form-group\">";
-        	$html .= "<label for=\"cargo\">Cargo</label>";
-        	$html .= "<input type=\"text\" id=\"cargo\" name=\"cargo\" class=\"form-control\"";
-        	$html .= "value=\"{$usuario['cargo']}\"/>";
-        	$html .= "</div>";
-        	$html .= "<div class=\"form-group\">";
-        	$html .= "<label for=\"fone\">Telefone / Ramal</label>";
-        	$html .= "<input type=\"text\" id=\"fone\" name=\"fone\" class=\"form-control\"";
-        	$html .= "value=\"{$usuario['telefone']}\"/>";
-        	$html .= "</div>";
-        	$html .= "<div class=\"form-group\">";
-        	$html .= "<label for=\"email\">E-mail</label>";
-        	$html .= "<input type=\"text\" id=\"email\" name=\"email\" class=\"form-control\"";
-        	$html .= "value=\"{$usuario['email']}\"/>";
-        	$html .= "</div>";
-        	$html .= "<div class=\"form-group\">";
-        	$html .= "<label for=\"login\">Login</label>";
-        	$html .= "<input type=\"text\" id=\"login\" name=\"login\" class=\"form-control\"";
-        	$html .= "value=\"{$usuario['login']}\" readonly/>";
-        	$html .= "</div>";
-        	$html .= "<div class=\"form-group\">";
-        	$html .= "<label for=\"senha\">Senha</label>";
-        	$html .= "<input type=\"password\" id=\"senha\" name=\"senha\" class=\"form-control\"";
-        	$html .= "</div>";
-        	$html .= "<div class=\"form-group\">";
-        	$html .= "<input type=\"hidden\" id=\"id\" 
-        			   name=\"id\" value=\"{$usuario['id']}\"/>";
-        	$html .= "<input type=\"hidden\" id=\"status\" 
-        	           name=\"status\" value=\"{$usuario['status']}\"/>";
-        	$html .= "<input type=\"hidden\" id=\"data_criacao\" 
-        	           name=\"data_criacao\" value=\"{$usuario['data_criacao']}\"/>";
-        	$html .= "<input type=\"hidden\" id=\"data_alteracao\" 
-        	           name=\"data_alteracao\" value=\"{$usuario['data_alteracao']}\"/>";
-        	$html .= "<input type=\"hidden\" id=\"tipo_usuario\" 
-        	           name=\"tipo_usuario\" value=\"{$usuario['tipo_usuario']}\"/>";
-        	$html .= "</div>";
-        	$html .= "<div class=\"btn-toolbar pull-right form-footer\">";
-        	$html .= "<button type=\"submit\" class=\"btn btn-success\">Salvar</button>";
-        	$html .= "<button type=\"reset\" class=\"btn btn-default\" onclick=\"limpar()\">Resetar</button>";
-        	$html .= "</div>";
-        	return $html;
+            $usuario = $this->getUsuario('s.id', $id);
+            $html = "<div><h2>Conta</h2></div>";
+            $html .= "<div class=\"form-group\">";
+            $html .= "<label for=\"nome\">Nome</label>";
+            $html .= "<input type=\"text\" id=\"nome\" name=\"nome\" class=\"form-control\"";
+            $html .= "value=\"{$usuario['nome']}\"/>";
+            $html .= "</div>";
+            $html .= "<div class=\"form-group\">";
+            $html .= "<label for=\"lotacao\">Lotação</label>";
+            $html .= "<select id=\"lotacao_id\" name=\"lotacao_id\" class=\"form-control\">";
+            $lotacoesController = new LotacoesController();
+            $lotacoes = $lotacoesController->_list();
+            foreach ($lotacoes as $lotacao) {
+                $html .= "<option value=\"{$lotacao['id']}\"> {$lotacao['nome']} </option>";
+            }
+            $html .= "</select></div>";
+            $html .= "<div class=\"form-group\">";
+            $html .= "<label for=\"cargo\">Cargo</label>";
+            $html .= "<input type=\"text\" id=\"cargo\" name=\"cargo\" class=\"form-control\"";
+            $html .= "value=\"{$usuario['cargo']}\"/>";
+            $html .= "</div>";
+            $html .= "<div class=\"form-group\">";
+            $html .= "<label for=\"fone\">Telefone / Ramal</label>";
+            $html .= "<input type=\"text\" id=\"fone\" name=\"fone\" class=\"form-control\"";
+            $html .= "value=\"{$usuario['telefone']}\"/>";
+            $html .= "</div>";
+            $html .= "<div class=\"form-group\">";
+            $html .= "<label for=\"email\">E-mail</label>";
+            $html .= "<input type=\"text\" id=\"email\" name=\"email\" class=\"form-control\"";
+            $html .= "value=\"{$usuario['email']}\"/>";
+            $html .= "</div>";
+            $html .= "<div class=\"form-group\">";
+            $html .= "<label for=\"login\">Login</label>";
+            $html .= "<input type=\"text\" id=\"login\" name=\"login\" class=\"form-control\"";
+            $html .= "value=\"{$usuario['login']}\" readonly/>";
+            $html .= "</div>";
+            $html .= "<div class=\"form-group\">";
+            $html .= "<label for=\"senha\">Senha</label>";
+            $html .= "<input type=\"password\" id=\"senha\" name=\"senha\" class=\"form-control\"";
+            $html .= "</div>";
+            $html .= "<div class=\"form-group\">";
+            $html .= "<input type=\"hidden\" id=\"id\"
+                name=\"id\" value=\"{$usuario['id']}\"/>";
+            $html .= "<input type=\"hidden\" id=\"status\"
+                name=\"status\" value=\"{$usuario['status']}\"/>";
+            $html .= "<input type=\"hidden\" id=\"data_criacao\"
+                name=\"data_criacao\" value=\"{$usuario['data_criacao']}\"/>";
+            $html .= "<input type=\"hidden\" id=\"data_alteracao\"
+                name=\"data_alteracao\" value=\"{$usuario['data_alteracao']}\"/>";
+            $html .= "<input type=\"hidden\" id=\"tipo_usuario\"
+                name=\"tipo_usuario\" value=\"{$usuario['tipo_usuario']}\"/>";
+            $html .= "</div>";
+            $html .= "<div class=\"btn-toolbar pull-right form-footer\">";
+            $html .= "<button type=\"submit\" class=\"btn btn-success\">Salvar</button>";
+            $html .= "<button type=\"reset\" class=\"btn btn-default\" onclick=\"limpar()\">Resetar</button>";
+            $html .= "</div>";
+            return $html;
         }
     }
 ?>
