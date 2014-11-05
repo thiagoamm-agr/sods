b1a99747234df3db1878e3b48d1112f1a0887b92
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-11-05 11:29:40 -0200

    1 - Formatação do código no padrão e correção. 2 - Utilização de disabled no lugar de readonly no campo ativar no formulário de cadastro.

--- admin/tiposSolicitacao/index.php
+++ admin/tiposSolicitacao/index.php
@@ -1,84 +1,82 @@
 <?php
-/* CADASTRO DE TIPO DE SOLICITAÇÕES */
+    // CADASTRO DE TIPO DE SOLICITAÇÕES
 
-// Model
-@include $_SERVER ['DOCUMENT_ROOT'] . '/sods/app/models/TipoSolicitacao.php';
+    // Model
+    @include $_SERVER['DOCUMENT_ROOT'] . '/sods/app/models/TipoSolicitacao.php';
 
-// Controller
-@include $_SERVER ['DOCUMENT_ROOT'] . '/sods/app/controllers/TiposSolicitacoesController.php';
+    // Controller
+    @include $_SERVER['DOCUMENT_ROOT'] . '/sods/app/controllers/TiposSolicitacoesController.php';
 
-$controller = new TiposSolicitacoesController ();
+    $controller = new TiposSolicitacoesController();
 
-// Identificação e atendimento das ações do usuário pelo controller.
-if (isset ( $_POST ['action'] )) {
-    // Recuperando os parâmetros da requisição.
-    $action = $_POST ['action'];
-    
-    if (isset ( $_POST ['json'] )) {
-        $json = $_POST ['json'];
-        if (! empty ( $json )) {
-            $tipoSolicitacao = new TipoSolicitacao ();
-            $tipoSolicitacao->loadJSON ( $json );
-        }
-    }
-    
-    // Identificando a ação desempenhada pelo usuário.
-    switch ($action) {
-        case 'add' :
-            $controller->add($tipoSolicitacao);
-            exit ();
-            break;
-        case 'edit' :
-            $controller->edit($tipoSolicitacao);
-            exit ();
-            break;
-        case 'delete' :
-            $fail = $controller->delete($tipoSolicitacao);
-            if($fail){
-                echo 'ERRO';
+    // Identificação e atendimento das ações do usuário pelo controller.
+    if (isset ($_POST['action'])) {
+        // Recuperando os parâmetros da requisição.
+        $action = $_POST['action'];
+        if (isset ($_POST['json'])) {
+            $json = $_POST['json'];
+            if (!empty($json)) {
+                $tipoSolicitacao = new TipoSolicitacao();
+                $tipoSolicitacao->loadJSON($json);
             }
-            exit();
-            break;
-        case 'list' :
-            echo $controller->getGrid ();
-            exit();
-            break;
+        }
+        // Identificando a ação desempenhada pelo usuário.
+        switch ($action) {
+            case 'add':
+                $controller->add($tipoSolicitacao);
+                exit();
+                break;
+            case 'edit':
+                $controller->edit($tipoSolicitacao);
+                exit();
+                break;
+            case 'delete':
+                $fail = $controller->delete($tipoSolicitacao);
+                if ($fail) {
+                    echo'ERRO';
+                }
+                exit();
+                break;
+            case 'list':
+                echo $controller->getGrid();
+                exit();
+                break;
     }
 }
 ?>
 <?php
-// Topo
-@include $_SERVER ['DOCUMENT_ROOT'] . '/sods/includes/topo.php';
+    // Topo
+    @include $_SERVER['DOCUMENT_ROOT'] . '/sods/includes/topo.php';
 ?>
-<!--  Javascript -->
-<script type="text/javascript"
-    src="/sods/static/js/models/TipoSolicitacao.js"></script>
-<script type="text/javascript"
-    src="/sods/static/js/validators/TipoSolicitacaoFormValidator.js"></script>
+    <!--  Javascript -->
+    <script type="text/javascript" src="/sods/static/js/models/TipoSolicitacao.js"></script>
+    <script type="text/javascript" src="/sods/static/js/validators/TipoSolicitacaoFormValidator.js"></script>
 
-<script type="text/javascript">
-             var tipoSolicitacao = null;
-             var action = null;
-             var form = null;
-             var formValidator = null;
-             var resposta=null;
+    <script type="text/javascript">
+        var tipoSolicitacao = null;
+        var action = null;
+        var form = null;
+        var formValidator = null;
+        var resposta = null;
 
-            function add() {
-                action = "add";
-                tipoSolicitacao = new TipoSolicitacao();
-                tipoSolicitacao.id = null;
-                form= $('#form-add');
-                formValidator = new TipoSolicitacaoFormValidator(form);
-            }
+        function add() {
+            action = "add";
+            tipoSolicitacao = new TipoSolicitacao();
+            tipoSolicitacao.id = null;
+            form = $('#form-add');
+            formValidator = new TipoSolicitacaoFormValidator(form);
+        }
 
-            function edit(tipoSolicitacao_json) {
-                try {
-                    if(tipoSolicitacao_json.status=='A'){
-                        document.getElementById('statusEdit').setAttribute("checked","checked");
-                    }else{
-                        document.getElementById('statusEdit').removeAttribute("checked");
-                    }
-                    if (tipoSolicitacao_json != null) {
+        function edit(tipoSolicitacao_json) {
+            try {
+                if (tipoSolicitacao_json.status == 'A') {
+                    //document.getElementById('statusEdit').setAttribute("checked","checked");
+                    $('#statusEdit').prop('checked', true);
+                } else {
+                    //document.getElementById('statusEdit').removeAttribute("checked");
+                    $('#statusEdit').prop('checked', false);
+                }
+                if (tipoSolicitacao_json != null) {
                         action = 'edit';
                         modal='modal-edit';
                         form = $('#form-' + action);
@@ -122,12 +120,12 @@ if (isset ( $_POST ['action'] )) {
                         action: 'list'
                     },
                     success: function(data, status, xhr) {
+                        console.log(data);
                         if (data == 'ERRO') {
                             $('#alert-del').modal('show');
                         } else {
                             $('#grid').html(data);
                         }
-                        console.log(data);
                     },
                     error: function(xhr, status, error) {
                         console.log(error);
@@ -146,7 +144,9 @@ if (isset ( $_POST ['action'] )) {
                         case 'edit':
                             tipoSolicitacao.nome = $('#nome', form).val();
                             tipoSolicitacao.status = $(
-                                '#form-' + action  + ' input:checkbox[name="status"]:checked'
+                               '#form-' + 
+                                action  + 
+                                ' input:checkbox[name="status"]:checked'
                             ).val();
                             if (tipoSolicitacao.status != "A") {
                                 tipoSolicitacao.status = "I";
@@ -171,14 +171,14 @@ if (isset ( $_POST ['action'] )) {
                             'json': json
                         },
                         success: function(data, status, xhr) {
+                            console.log(data);
                             if (data == 'ERRO') {
                                 $('#alert-del').modal('show');
                             }
-                            console.log(data);
                             list();
                         },
                         error: function(xhr, status, error) {
-                            //console.log(error);
+                            console.log(error);
                         },
                         complete: function(xhr, status) {
                             //console.log('A requisição foi completada.');
@@ -229,46 +229,54 @@ if (isset ( $_POST ['action'] )) {
             </div>
             <div id="grid" class="table-responsive">
 <?php
-                    if (isset ( $_GET ['p'] )) {
-                        $page = ( int ) $_GET ['p'];
+                    if (isset($_GET['p'])) {
+                        $page = (int) $_GET['p'];
                     } else {
                         $page = 1;
                     }
-                    echo $controller->getGrid ( $page );
+                    echo $controller->getGrid($page);
 ?>
             </div>
 
     <!-- Modais -->
 
     <!-- Adicionar Tipo de Solicitação -->
-    <div class="modal fade" id="modal-add" tabindex="-1" role="dialog"
-        aria-labelledby="modal-add" aria-hidden="true">
+    <div id="modal-add" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="modal-add" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal"
-                        aria-hidden="true">&times;</button>
-                    <h3 class="modal-title" id="modal-add">Adicionar Tipo de
-                        Solicitação</h3>
+                    <button 
+                        type="button" 
+                        class="close" 
+                        data-dismiss="modal"
+                        aria-hidden="true">&times;
+                    </button>
+                    <h3 id="modal-add" class="modal-title">Adicionar Tipo de Solicitação</h3>
                 </div>
                 <div class="modal-body">
-                    <form id="form-add" action="/sods/admin/tiposSolicitacao/"
-                        method="post">
+                    <form id="form-add" action="/sods/admin/tiposSolicitacao/" method="post">
                         <div class="form-group">
-                            <label for="nome">Nome</label> <input type="text"
-                                class="form-control" name="nome" id="nome" maxlength="80" />
+                            <label for="nome">Nome</label>
+                            <input 
+                                type="text" 
+                                id="nome" 
+                                name="nome" 
+                                class="form-control" 
+                                maxlength="80" />
                         </div>
                         <div class="form-group">
-                            <input type="checkbox" name="status" id=status value="A" checked="checked"
-                                readonly />&nbsp; Ativar
+                            <input 
+                                type="checkbox" 
+                                id=status 
+                                name="status" 
+                                value="A" 
+                                checked="checked" 
+                                disabled />&nbsp; Ativar
                         </div>
                         <div class="modal-footer">
-                            <button type="submit" class="btn btn-success">Salvar
-                            </button>
-                            <button type="reset" class="btn btn-default" onclick="resetForm()">Limpar
-                            </button>
-                            <button type="button" class="btn btn-primary"
-                                data-dismiss="modal">Cancelar</button>
+                            <button type="submit" class="btn btn-success">Salvar</button>
+                            <button type="reset" class="btn btn-default" onclick="resetForm()">Limpar</button>
+                            <button type="button" class="btn btn-primary" data-dismiss="modal">Cancelar</button>
                         </div>
                     </form>
                 </div>
@@ -277,36 +285,40 @@ if (isset ( $_POST ['action'] )) {
     </div>
 
     <!-- Editar Tipo de Solicitação-->
-    <div class="modal fade" id="modal-edit" tabindex="-1" role="dialog"
-        aria-labelledby="modal-edit" aria-hidden="true">
+    <div id="modal-edit" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="modal-edit" 
+        aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal"
-                        aria-hidden="true">&times;</button>
-                    <h3 class="modal-title" id="modal-edit">Editar Tipo de Solicitação</h3>
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h3 id="modal-edit" class="modal-title">Editar Tipo de Solicitação</h3>
                 </div>
                 <div class="modal-body">
-                    <form id="form-edit" action="/sods/admin/tiposSolicitacao/"
-                        method="post">
+                    <form id="form-edit" action="/sods/admin/tiposSolicitacao/" method="post">
                         <div class="form-group">
-                            <label for="nome">Nome</label> <input type="text" id="nome"
-                                name="nome" class="form-control" maxlength="80" />
+                            <label for="nome">Nome</label>
+                            <input 
+                                type="text" 
+                                id="nome" 
+                                name="nome" 
+                                class="form-control" 
+                                maxlength="80" />
                         </div>
                         <div>
                             <label for="status">Status</label>
                         </div>
                         <div class="form-group">
-                            <input id="statusEdit" type="checkbox" name="status" value="A"
+                            <input 
+                                id="statusEdit" 
+                                type="checkbox" 
+                                name="status" 
+                                value="A" 
                                 checked="checked" />&nbsp; Ativar
                         </div>
                         <div class="modal-footer">
-                            <button type="submit" class="btn btn-success" >
-                                Salvar</button>
-                            <button type="reset" class="btn btn-default" onclick="resetForm()">
-                                Limpar</button>
-                            <button type="button" class="btn btn-primary"
-                                data-dismiss="modal">Cancelar</button>
+                            <button type="submit" class="btn btn-success" >Salvar</button>
+                            <button type="reset" class="btn btn-default" onclick="resetForm()">Limpar</button>
+                            <button type="button" class="btn btn-primary" data-dismiss="modal">Cancelar</button>
                         </div>
                     </form>
                 </div>
@@ -315,18 +327,15 @@ if (isset ( $_POST ['action'] )) {
     </div>
 
     <!-- Excluir Tipo de Solicitação -->
-    <div class="modal fade" id="modal-del" tabindex="-1" role="dialog"
-        aria-labelledby="modal-del" aria-hidden="true">
+    <div id="modal-del" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="modal-del" 
+        aria-hidden="true">
         <div class="modal-dialog modal-sm">
             <div class="modal-content">
                 <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal"
-                        aria-hidden="true">&times;</button>
-                    <h4 class="modal-title" id="modal-del">Exclusão de Tipo de
-                        Solicitação</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title" id="modal-del">Exclusão de Tipo de Solicitação</h4>
                 </div>
-                <form id="form-del" action="/sods/admin/tiposSolicitacao/"
-                    method="post">
+                <form id="form-del" action="/sods/admin/tiposSolicitacao/" method="post">
                     <div class="modal-body">
                         <h5>Confirma a exclusão?</h5>
                     </div>
@@ -338,22 +347,20 @@ if (isset ( $_POST ['action'] )) {
             </div>
         </div>
     </div>
-    <!-- Modais -->
-    <!-- Alerta -->
-    <div class="modal fade" id="alert-del" tabindex="-1" role="dialog"
-        aria-labelledby="modal-del" aria-hidden="true">
+
+    <!-- Alertas -->
+    <div id="alert-del" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="modal-del" aria-hidden="true">
         <div class="alert alert-danger fade in" role="alert">
-            <button type="button" class="close"
-                onclick="$('#alert-del').modal('toggle');">
+            <button type="button" class="close" onclick="$('#alert-del').modal('toggle');">
                 <span aria-hidden="true">&times;</span><span class="sr-only">Fechar</span>
             </button>
-            <strong>ERRO:</strong> Não é possível excluir um registro com
-            referências.
+            <strong>ERRO:</strong> Não é possível excluir um registro com referências.
         </div>
     </div>
 </div>
 <!-- Container -->
+
 <?php
-// Rodapé
-@include $_SERVER ['DOCUMENT_ROOT'] . '/sods/includes/rodape.php';
+    // Rodapé
+    @include $_SERVER['DOCUMENT_ROOT'] . '/sods/includes/rodape.php';
 ?>
