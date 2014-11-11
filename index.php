20fbc091a80a3f37aa6147648b3085534a501b07
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-11-11 16:58:48 -0200

    Removidas linhas em branco.

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
         <link rel="stylesheet" type="text/css" media="screen" href="/sods/static/bootstrap/css/bootstrap.min.css" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
+
         <!-- Estilo para esse template -->
         <link href="/sods/static/bootstrap/css/signin.css" rel="stylesheet" />
-        
+
         <title>Login</title>
     </head>
         <body>
@@ -34,7 +34,7 @@
                 </div> 
 <?php
     @require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/lib/auth.php';
-            
+
     @$login = $_POST['login'];
     @$senha = $_POST['senha'];
 
