2cff70b01ccd50f67d4799f215ec20884cf380ed
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-11-17 16:03:43 -0200

    Aplicada a formatação de 4 espaços (padrão) ao código-fonte.

--- app/controllers/UsuariosController.php
+++ app/controllers/UsuariosController.php
@@ -29,8 +29,8 @@
         }
 
         public function add($usuario) {
-        	$usuario->senha = $this->generatePassword();
-        	$this->sendPassword();
+            $usuario->senha = $this->generatePassword();
+            $this->sendPassword();
             $this->dao->insert($usuario);
         }
         
@@ -53,48 +53,35 @@
         }
         
         public function sendPassword(){
-	        //SMTP needs accurate times, and the PHP time zone MUST be set
-			//This should be done in your php.ini, but this is how to do it if you don't have access to that
-			//date_default_timezone_set('Etc/UTC');
-			//Create a new PHPMailer instance
-			$mail = new PHPMailer;
-			//Tell PHPMailer to use SMTP
-			$mail->isSMTP();
-			//Enable SMTP debugging
-			// 0 = off (for production use)
-			// 1 = client messages
-			// 2 = client and server messages
-			//$mail->SMTPDebug = 2;
-			//Ask for HTML-friendly debug output
-			//$mail->Debugoutput = 'html';
-			//Set the hostname of the mail server
-			$mail->Host = 'smtp.gmail.com';
-			//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-			$mail->Port = 587;
-			//Set the encryption system to use - ssl (deprecated) or tls
-			$mail->SMTPSecure = 'tls';
-			//Whether to use SMTP authentication
-			$mail->SMTPAuth = true;
-			//Username to use for SMTP authentication - use full email address for gmail
-			$mail->Username = "?????????";
-			//Password to use for SMTP authentication
-			$mail->Password = "?????????";
-			// Remetente.
-			$mail->setFrom('?????????', '?????????');
-			// Destinatários.
-			$mail->addAddress('?????????', '?????????');
-			$mail->addAddress('?????????', '?????????');
-			// Assunto.
-			$mail->Subject = '?????????';
-			// Configurando o corpo da mensagem.
-			$mail->isHTML(true);
-		    $mail->Body = 'Este é o corpo da mensagem de teste, em <b>HTML</b>! <br>';
-			$mail->AltBody = 'This is a plain-text message body';
-			if (!$mail->send()) {
-			    echo "Mailer Error: " . $mail->ErrorInfo;
-			} else {
-			    echo "Message sent!";
-			}
+            $mail = new PHPMailer;
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->Port = 587;
+            // Criptografia - TLS ou SSL (depreciada).
+            $mail->SMTPSecure = 'tls';
+            $mail->SMTPAuth = true;
+            // Usuário.
+            $mail->Username = "??????";
+            // Senha.
+            $mail->Password = "??????";
+            // Remetente.
+            $mail->setFrom('??????', '??????');
+            // Destinatários.
+            $mail->addAddress('??????', '??????');
+            $mail->addAddress('??????', '??????');
+            // Assunto.
+            $mail->Subject = '??????';
+            // Corpo da mensagem.
+            $mail->isHTML(true);
+            // Mensagem em hipertexto.
+            $mail->Body = 'Este é o corpo da mensagem de teste, em <b>HTML</b>! <br>';
+            // Mensagem em texto puro.
+            $mail->AltBody = 'This is a plain-text message body';
+            if (!$mail->send()) {
+                echo "Mailer Error: " . $mail->ErrorInfo;
+            } else {
+                echo "Message sent!";
+            }
         }
 
         public function delete($usuario) {
@@ -125,7 +112,8 @@
         public function getGrid($page_number=1, $usuarios=null) {
             $usuarios = empty($usuarios) ? $this->getPage($page_number) : $usuarios;
             if (count($usuarios) > 0) {
-                $html = "<table class=\"table table-striped table-bordered table-condensed tablesorter tablesorter-default\" role=\"grid\"";
+                $html = "<table class=\"table table-striped table-bordered table-condensed tablesorter " . 
+                "tablesorter-default\" role=\"grid\"";
                 $html .= "id=\"tablesorter\">";
                 $html .= "<thead><tr>";
                 $html .= "<th>ID</th>";
