cd47661516563f253b7580a0413facd7a473161b
manoel-an <manoelanagr@gmail.com>
2014-11-17 15:45:04 -0200

    Código para envio de email.

--- app/controllers/UsuariosController.php
+++ app/controllers/UsuariosController.php
@@ -4,6 +4,8 @@
     @require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/dao/UsuarioDAO.php';
 
     @include_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/controllers/SolicitacoesController.php';
+    @include_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/lib/class.phpmailer.php';
+    @include_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/lib/class.smtp.php';
 
     class UsuariosController extends Controller {
 
@@ -27,7 +29,72 @@
         }
 
         public function add($usuario) {
+        	$usuario->senha = $this->generatePassword();
+        	$this->sendPassword();
             $this->dao->insert($usuario);
+        }
+        
+        public function generatePassword(){
+            $tamanho = 8;
+            $alfabeto = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+            $minimo = 0;
+            $maximo = strlen($alfabeto) - 1;
+            // Gerando a sequencia
+            $sequencia = '';
+            for ($i = $tamanho; $i > 0; --$i) {
+                // Sorteando uma posicao do alfabeto
+                $posicao_sorteada = mt_rand($minimo, $maximo);
+                // Obtendo o simbolo correspondente do alfabeto
+                $caractere_sorteado = $alfabeto[$posicao_sorteada];
+                // Incluindo o simbolo na sequencia
+                $sequencia .= $caractere_sorteado;
+            }
+            return $sequencia;
+        }
+        
+        public function sendPassword(){
+	        //SMTP needs accurate times, and the PHP time zone MUST be set
+			//This should be done in your php.ini, but this is how to do it if you don't have access to that
+			//date_default_timezone_set('Etc/UTC');
+			//Create a new PHPMailer instance
+			$mail = new PHPMailer;
+			//Tell PHPMailer to use SMTP
+			$mail->isSMTP();
+			//Enable SMTP debugging
+			// 0 = off (for production use)
+			// 1 = client messages
+			// 2 = client and server messages
+			//$mail->SMTPDebug = 2;
+			//Ask for HTML-friendly debug output
+			//$mail->Debugoutput = 'html';
+			//Set the hostname of the mail server
+			$mail->Host = 'smtp.gmail.com';
+			//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+			$mail->Port = 587;
+			//Set the encryption system to use - ssl (deprecated) or tls
+			$mail->SMTPSecure = 'tls';
+			//Whether to use SMTP authentication
+			$mail->SMTPAuth = true;
+			//Username to use for SMTP authentication - use full email address for gmail
+			$mail->Username = "?????????";
+			//Password to use for SMTP authentication
+			$mail->Password = "?????????";
+			// Remetente.
+			$mail->setFrom('?????????', '?????????');
+			// Destinatários.
+			$mail->addAddress('?????????', '?????????');
+			$mail->addAddress('?????????', '?????????');
+			// Assunto.
+			$mail->Subject = '?????????';
+			// Configurando o corpo da mensagem.
+			$mail->isHTML(true);
+		    $mail->Body = 'Este é o corpo da mensagem de teste, em <b>HTML</b>! <br>';
+			$mail->AltBody = 'This is a plain-text message body';
+			if (!$mail->send()) {
+			    echo "Mailer Error: " . $mail->ErrorInfo;
+			} else {
+			    echo "Message sent!";
+			}
         }
 
         public function delete($usuario) {
