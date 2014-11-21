95b89e1a352f93b6f2bd5fa63f5a9dbbac947d82
Guthierrez <guthi.inf@gmail.com>
2014-11-21 15:12:36 -0200

    Crianção da tela sobre

---
+++ admin/sobre.php
@@ -0,0 +1,39 @@
+<?php
+    @include $_SERVER['DOCUMENT_ROOT'] . '/sods/includes/topo.php';
+?>
+         <div class="container">
+             <div class="col-md-12">
+                <div class="page-header">
+                    <h1>Sobre</h1>
+                </div>
+                <h3><strong>SODS </strong>-<span class="text-muted"> Sistema para Solicitação de Desenvolvimento de 
+                    Software</span></h3>
+                <p>&nbsp;</p>
+                <p>
+                    O <strong>SODS</strong> é um sistema concebido para controle das solicitações de desenvolvimento de 
+                    software na <strong>AGR</strong>.  
+                </p>
+                <p>
+                    Tem como intuito facilitar o processo de criação de demandas por parte das coordenações e gerências 
+                    e atendimento das mesmas pela equipe de desenvolvimento.
+                </p>
+                <p>Autores:</p>
+                <ul>
+                    <li>Thiago Alexandre Martins Monteiro</li>
+                    <li>Edward Neto</li>
+                    <li>Guthierrez Gregório de Souza</li>
+                    <li>Manoel Albino Neto</li>
+                </ul>
+             </div>
+             <footer class="footer">
+                <div class="container">
+                    <p class="text-muted" style="text-align:center">
+                        <strong><a href="http://www.agr.go.gov.br/" target="blank" style="color:blue">AGR</a></strong> 
+                        - Agência Goiana de Reulação,Controle e Fiscalização de Serviços Públicos - 2014
+                    </p>
+                </div>
+             </footer>
+      </div>
+<?php
+    @include $_SERVER ['DOCUMENT_ROOT'] . '/sods/includes/rodape.php';
+?>
