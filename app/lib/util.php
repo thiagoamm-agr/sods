3f59034a28bece34b40a5fda48ae706624698374
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-11-21 18:17:01 -0200

    Adicionada restrição de acesso para o usuário comum.

--- app/lib/util.php
+++ app/lib/util.php
@@ -1,4 +1,7 @@
 <?php
+
+    @session_start();
+
     // returns true if $str begins with $sub
     function startsWith($str, $sub) {
         return (substr($str, 0, strlen($sub)) == $sub);
@@ -14,6 +17,19 @@
         $uri = $_SERVER['REQUEST_URI'];
         if (startsWith($uri, '/sods/app') || startswith($uri, '/sods/includes')) {
             header('location: /sods/404.html');
+        }
+        $tipo_usuario = $_SESSION['usuario']['tipo_usuario'];
+        if ($tipo_usuario == 'U') {
+            switch ($uri) {
+                case '/sods/admin/lotacoes/':
+                case '/sods/admin/lotacoes/index.php':
+                case '/sods/admin/usuarios/':
+                case '/sods/admin/usuarios/index.php':
+                case '/sods/admin/tiposSolicitacao/':
+                case '/sods/admin/tiposSolicitacao/index.php':
+                    header('location: /sods/404.html');
+                    break;
+            }
         }
     }
 
