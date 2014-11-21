de4e2b2edd5d0c11affa3bd96974f2f36b896c2a
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-11-21 15:41:53 -0200

    1 - Remoção de comentários desnecessários. 2 - Formatação do código.

--- app/controllers/SolicitacoesController.php
+++ app/controllers/SolicitacoesController.php
@@ -6,116 +6,17 @@
     class SolicitacoesController extends Controller {
 
         public function __construct() {
-            //faz chamada ao construtor da superclasse
+            // Faz chamada ao construtor da superclasse
             parent::__construct();
             $this->dao = new SolicitacaoDAO();
             $this->paginator->totalrecords = $this->dao->count();
-            // URL para requisições sem AJAX.
-            //$this->paginator->defaultUrl = "/sods/admin/solicitacoes/";
-            //$this->paginator->paginationUrl = "/sods/admin/solicitacoes/index.php?p=[p]";
-            // URL para requisições com AJAX.
             $this->paginator->defaultUrl = "/sods/admin/solicitacoes/";
             $this->paginator->paginationUrl = "/sods/admin/solicitacoes/";
         }
-        
-        public function getGrid($page_number=1, $filter=null, $value=null) {
-            $criteria = null;
-            //if (gettype($value) == 'string'
-            if (!empty($filter) && !empty($value)) {
-                switch ($filter) {
-                    case 'id':
-                    	$value = intval($value);
-                        $criteria = "so.{$filter} = {$value}";
-                        break;
-                    case 'nome':
-                        $criteria = "s.{$filter} LIKE '%{$value}%'";
-                        break;
-                    case 'titulo':
-                        $criteria = "so.{$filter} LIKE '%{$value}%'";
-                        break;
-                    case 'tipo':
-                        $criteria = "t.nome LIKE '%{$value}%'";
-                        break;
-                }
-            }            
-            $solicitacoes = $this->getPage($page_number, 10, $criteria);
-            $total_records = empty($criteria) ? $this->dao->count() : $this->dao->count($criteria);
-            $this->paginator->totalrecords = $total_records;
-            if ($total_records > 0) {
-                $html = "<table id=\"tablesorter\"\n";
-                $html .= "    class=\"table table-striped table-bordered table-condensed tablesorter\">\n";
-                $html .= "    <thead>\n";
-                $html .= "        <tr>\n";
-                $html .= "            <th width=\"5%\">ID</th>\n";
-                $html .= "            <th width=\"10%\">Solicitante</th>\n";
-                $html .= "            <th width=\"22%\">Titulo</th>\n";
-                $html .= "            <th width=\"10%\">Status</th>\n";
-                $html .= "            <th width=\"18%\">Tipo</th>\n";
-                $html .= "            <th width=\"9%\">Abertura</th>\n";
-                $html .= "            <th width=\"9%\">Alteração</th>\n";
-                $html .= "            <th width=\"17%\" class=\"nonSortable\">Ação</th>\n";
-                $html .= "        </tr>\n";
-                $html .= "    </thead>\n";
-                $html .= "    <tbody>\n";
-                foreach ($solicitacoes as $solicitacao) {
-                    $html .= "        <tr>\n";
-                    $html .= "            <td>{$solicitacao['id']}</td>\n";
-                    $html .= "            <td><span data-toggle=\"tooltip\" data-placement=\"bottom\" title=\"{$solicitacao['nome']} \">{$solicitacao['solicitante']}</span></td>\n";
-                    $html .= "            <td>{$solicitacao['titulo']}</td>\n";
-                    $html .= "            <td>{$solicitacao['status']}</td>\n";
-                    $html .= "            <td>{$solicitacao['tipo_solicitacao']}</td>\n";
-                    if ($solicitacao['data_abertura'] != null){
-                        $data_abertura_simples = date('d/m/Y', strtotime ($solicitacao['data_abertura']));
-                        $data_abertura_hora = date('H:i:s', strtotime ($solicitacao['data_abertura']));
-                        $html .= "            <td><span data-toggle=\"tooltip\" data-placement=\"bottom\" title=\"Data: $data_abertura_simples 
-                        Hora: $data_abertura_hora \">$data_abertura_simples</span></td>\n";
-                    }else{
-                        $html .= "            <td></td>\n";
-                    }
-                    if ($solicitacao['data_alteracao'] != null){
-                        $data_alteracao_hora = date('H:i:s', strtotime ($solicitacao['data_alteracao']));
-                        $data_alteracao_simples = date('d/m/Y', strtotime ($solicitacao['data_alteracao']));
-                        $html .= "            <td><span data-toggle=\"tooltip\" data-placement=\"bottom\" title=\"Data: $data_alteracao_simples 
-                        Hora: $data_alteracao_hora \">$data_alteracao_simples</span></td>\n";
-                    }else{
-                        $html .= "            <td></td>\n";
-                    }
-                    $html .= "            <td colspan=\"2\">\n";
-                    $html .= "                <button\n";
-                    $html .= "                    class=\"btn btn-warning btn-sm\"\n";
-                    $html .= "                    data-toggle=\"modal\"\n";
-                    $html .= "                    data-target=\"#modal-edit\"\n";
-                    $html .= "                    onclick='edit(" . json_encode($solicitacao) ."," . $page_number .")'>\n"; 
-                    $html .= "                    <strong>Editar&nbsp;<span class=\"glyphicon glyphicon-edit\"></span></strong>\n";
-                    $html .= "                </button>&nbsp;&nbsp;\n";
-                    $html .= "                <button\n";
-                    $html .= "                    class=\"delete-type btn btn-danger btn-sm\"\n";
-                    $html .= "                    data-toggle=\"modal\"\n";
-                    $html .= "                    data-target=\"#modal-del\"\n";
-                    $html .= "                    onclick='del(" . json_encode($solicitacao) . "," . $page_number . "," .$total_records .")'>\n"; 
-                    $html .= "                    <strong>Excluir&nbsp;<span class=\"glyphicon glyphicon-remove\"></span></strong>\n";
-                    $html .= "                </button>\n";
-                    $html .= "            </td>\n";
-                    $html .= "        </tr>\n";
-                }
-                $html .= "    </tbody>\n";
-                if ($total_records > 10) {
-                    $html .= "<tfoot>\n";
-                    $html .= "    <tr><td colspan=\"8\">{$this->paginator}</td></tr>\n";
-                    $html .= "</tfoot>\n";
-                }
-                $html .= "</table>\n";
-            } else {
-                $html = "<div class=\"alert alert-danger\" role=\"alert\">\n";
-                $html .= "    <center><b>Não há registros de solicitações.</b></center>\n";
-                $html .= "</div>\n";
-            }
-            return $html;
-        }        
 
         public function __destruct() {
             unset($this->dao);
-            //faz chamada ao destrutor da superclasse
+            // Faz chamada ao destrutor da superclasse
             parent::__destruct();
         }
 
@@ -150,6 +51,100 @@
             $this->paginator->pagesize = $rows;
             $this->paginator->pagenumber = $page_number;
             return $this->dao->paginate($rows, $page_number * $rows - $rows, $criteria);
+        }
+
+        public function getGrid($page_number=1, $filter=null, $value=null) {
+            $criteria = null;
+            if (!empty($filter) && !empty($value)) {
+                switch ($filter) {
+                    case 'id':
+                        $value = intval($value);
+                        $criteria = "so.{$filter} = {$value}";
+                        break;
+                    case 'nome':
+                        $criteria = "s.{$filter} LIKE '%{$value}%'";
+                        break;
+                    case 'titulo':
+                        $criteria = "so.{$filter} LIKE '%{$value}%'";
+                        break;
+                    case 'tipo':
+                        $criteria = "t.nome LIKE '%{$value}%'";
+                        break;
+                }
+            }
+            $solicitacoes = $this->getPage($page_number, 10, $criteria);
+            $total_records = empty($criteria) ? $this->dao->count() : $this->dao->count($criteria);
+            $this->paginator->totalrecords = $total_records;
+            if ($total_records > 0) {
+                $html = "<table id=\"tablesorter\"\n";
+                $html .= "    class=\"table table-striped table-bordered table-condensed tablesorter\">\n";
+                $html .= "    <thead>\n";
+                $html .= "        <tr>\n";
+                $html .= "            <th width=\"5%\">ID</th>\n";
+                $html .= "            <th width=\"10%\">Solicitante</th>\n";
+                $html .= "            <th width=\"22%\">Titulo</th>\n";
+                $html .= "            <th width=\"10%\">Status</th>\n";
+                $html .= "            <th width=\"18%\">Tipo</th>\n";
+                $html .= "            <th width=\"9%\">Abertura</th>\n";
+                $html .= "            <th width=\"9%\">Alteração</th>\n";
+                $html .= "            <th width=\"17%\" class=\"nonSortable\">Ação</th>\n";
+                $html .= "        </tr>\n";
+                $html .= "    </thead>\n";
+                $html .= "    <tbody>\n";
+                foreach ($solicitacoes as $solicitacao) {
+                    $html .= "        <tr>\n";
+                    $html .= "            <td>{$solicitacao['id']}</td>\n";
+                    $html .= "            <td><span data-toggle=\"tooltip\" data-placement=\"bottom\" title=\"{$solicitacao['nome']} \">{$solicitacao['solicitante']}</span></td>\n";
+                    $html .= "            <td>{$solicitacao['titulo']}</td>\n";
+                    $html .= "            <td>{$solicitacao['status']}</td>\n";
+                    $html .= "            <td>{$solicitacao['tipo_solicitacao']}</td>\n";
+                    if ($solicitacao['data_abertura'] != null){
+                        $data_abertura_simples = date('d/m/Y', strtotime ($solicitacao['data_abertura']));
+                        $data_abertura_hora = date('H:i:s', strtotime ($solicitacao['data_abertura']));
+                        $html .= "            <td><span data-toggle=\"tooltip\" data-placement=\"bottom\" title=\"Data: $data_abertura_simples 
+                        Hora: $data_abertura_hora \">$data_abertura_simples</span></td>\n";
+                    }else{
+                        $html .= "            <td></td>\n";
+                    }
+                    if ($solicitacao['data_alteracao'] != null) {
+                        $data_alteracao_hora = date('H:i:s', strtotime ($solicitacao['data_alteracao']));
+                        $data_alteracao_simples = date('d/m/Y', strtotime ($solicitacao['data_alteracao']));
+                        $html .= "            <td><span data-toggle=\"tooltip\" data-placement=\"bottom\" title=\"Data: $data_alteracao_simples 
+                        Hora: $data_alteracao_hora \">$data_alteracao_simples</span></td>\n";
+                    } else {
+                        $html .= "            <td></td>\n";
+                    }
+                    $html .= "            <td colspan=\"2\">\n";
+                    $html .= "                <button\n";
+                    $html .= "                    class=\"btn btn-warning btn-sm\"\n";
+                    $html .= "                    data-toggle=\"modal\"\n";
+                    $html .= "                    data-target=\"#modal-edit\"\n";
+                    $html .= "                    onclick='edit(" . json_encode($solicitacao) ."," . $page_number .")'>\n"; 
+                    $html .= "                    <strong>Editar&nbsp;<span class=\"glyphicon glyphicon-edit\"></span></strong>\n";
+                    $html .= "                </button>&nbsp;&nbsp;\n";
+                    $html .= "                <button\n";
+                    $html .= "                    class=\"delete-type btn btn-danger btn-sm\"\n";
+                    $html .= "                    data-toggle=\"modal\"\n";
+                    $html .= "                    data-target=\"#modal-del\"\n";
+                    $html .= "                    onclick='del(" . json_encode($solicitacao) . "," . $page_number . "," .$total_records .")'>\n"; 
+                    $html .= "                    <strong>Excluir&nbsp;<span class=\"glyphicon glyphicon-remove\"></span></strong>\n";
+                    $html .= "                </button>\n";
+                    $html .= "            </td>\n";
+                    $html .= "        </tr>\n";
+                }
+                $html .= "    </tbody>\n";
+                if ($total_records > 10) {
+                    $html .= "<tfoot>\n";
+                    $html .= "    <tr><td colspan=\"8\">{$this->paginator}</td></tr>\n";
+                    $html .= "</tfoot>\n";
+                }
+                $html .= "</table>\n";
+            } else {
+                $html = "<div class=\"alert alert-danger\" role=\"alert\">\n";
+                $html .= "    <center><b>Não há registros de solicitações.</b></center>\n";
+                $html .= "</div>\n";
+            }
+            return $html;
         }
     }
 ?>
