0d5b678a69f58f17459886e1f366721f6e0657cc
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-11-25 14:48:22 -0200

    1 - Alterações no nome das colunas cargo e tipo_usuario na tabela de solicitante para funcao e perfil, respectivamente.
    2 - Criação da tabela historico_solicitacao.

--- app/scripts/sods.sql
+++ app/scripts/sods.sql
@@ -21,38 +21,38 @@ flush privileges;
 use sods_development;
 
 -- Tabelas.
-create table lotacao(
+create table lotacao (
     id int not null primary key auto_increment,
     nome varchar(67) not null unique,
     sigla varchar(10) not null unique,
     gerencia_id int
 ) character set utf8 collate utf8_general_ci;
 
-create table solicitante(
+create table solicitante (
     id int not null primary key auto_increment,
     nome varchar(255) not null, 
     lotacao_id int not null,
-    cargo varchar(255) not null,
+    funcao varchar(255) not null,
     telefone varchar(30),
     email varchar(100),
     login varchar(50) not null unique,
     senha varchar(255) not null,
-    tipo_usuario char not null default 'U' comment 'U - Usuário ou A - Administrador',
+    perfil char not null default 'P' comment 'A - Admin ou P - Padrão',
     status char not null default 'A' comment 'A - Ativo ou I - Inativo',
     data_criacao timestamp not null default current_timestamp,
-    data_alteracao timestamp,
-    check (tipo_usuario in ('U', 'A')),
+    data_alteracao timestamp not null default timestamp on update current_timestamp,
+    check (perfil in ('A', 'P')),
     check (status in ('A', 'I'))
 ) character set utf8 collate utf8_general_ci;
 
-create table tipo_solicitacao(
+create table tipo_solicitacao (
     id int not null primary key auto_increment,
     nome varchar(80) not null unique,
     status char not null default 'A' comment 'A - Ativo ou I - Inativo',
     check (status in ('A', 'I'))
 ) character set utf8 collate utf8_general_ci;
 
-create table solicitacao(
+create table solicitacao (
     id int not null primary key auto_increment,
     solicitante_id int not null,
     titulo varchar(100) not null,
@@ -61,8 +61,24 @@ create table solicitacao(
     observacoes text,
     status varchar(15) not null default 'CRIADA',
     observacoes_status text,
-    data_abertura timestamp not null default current_timestamp,
-    data_alteracao datetime,
+    data_criacao timestamp not null default current_timestamp,
+    data_alteracao timestamp not null default timestamp on update current_timestamp,
+    tipo_solicitacao_id int not null,
+    check(status in ('CRIADA', 'EM ANÁLISE', 'DEFERIDA', 'INDEFERIDA', 'ATENDIDA', 'CANCELADA'))
+) character set utf8 collate utf8_general_ci;
+
+create table historico_solicitacao (
+    id int not null primary key auto_increment,
+    solicitacao_id int not null,
+    solicitante_id int not null,
+    titulo varchar(100) not null,
+    detalhamento text not null,
+    info_adicionais text,
+    observacoes text,
+    status varchar(15) not null default 'CRIADA',
+    observacoes_status text,
+    data_criacao timestamp not null default current_timestamp,
+    data_alteracao timestamp not null default timestamp on update current_timestamp,
     tipo_solicitacao_id int not null,
     check(status in ('CRIADA', 'EM ANÁLISE', 'DEFERIDA', 'INDEFERIDA', 'ATENDIDA', 'CANCELADA'))
 ) character set utf8 collate utf8_general_ci;
@@ -76,9 +92,22 @@ alter table lotacao add constraint fk_gerencia_id
 -- solicitante
 alter table solicitante add constraint fk_lotacao_id 
     foreign key(lotacao_id) references lotacao(id);
+
+-- solicitacao
 alter table solicitacao add constraint fk_solicitante_id 
     foreign key(solicitante_id) references solicitante(id);
+
 alter table solicitacao add constraint fk_tipo_solicitacao_id 
+    foreign key(tipo_solicitacao_id) references tipo_solicitacao(id);
+
+-- historico_solicitacao
+alter table historico_solicitacao add constraint fk_solicitacao_id
+    foreign key(solicitacao_id) references solicitacao(id);
+
+alter table historico_solicitacao add constraint fk_solicitante_id
+    foreign key(solicitante_id) references solicitante(id);
+
+alter table historico_solicitacao add constraint fk_tipo_solicitacao_id
     foreign key(tipo_solicitacao_id) references tipo_solicitacao(id);
 
 -- Inserção de dados
@@ -88,12 +117,12 @@ insert into lotacao(nome, sigla, gerencia_id) values ('Gerência de Gestão e Pl
 insert into lotacao(nome, sigla, gerencia_id) values('Coordenação de Informática', 'CI', 1);
 
 -- solicitante
-insert into solicitante(nome, lotacao_id, cargo, telefone, email, login, senha) 
+insert into solicitante(nome, lotacao_id, funcao, telefone, email, login, senha) 
     values('Bruno', 1, 'Gerente', '(62) 4545-4554', 'bruno@agr.go.gov.br', 'bruno', md5('bruno'));
-insert into solicitante(nome, lotacao_id, cargo, telefone, email, login, senha, tipo_usuario) 
+insert into solicitante(nome, lotacao_id, funcao, telefone, email, login, senha, perfil) 
     values('Thiago', 1, 'Programador', '(62) 4545-4554', 'thiago.amm.agr@gmail.com', 'thiago', md5('thiago'), 'A');
-insert into solicitante(nome, lotacao_id, cargo, telefone, email, login, senha, tipo_usuario) 
+insert into solicitante(nome, lotacao_id, cargo, telefone, email, login, senha, perfil) 
     values('Neto', 1, 'Programador', '(62) 4545-4554', 'edward-arn@agr.go.gov.br', 'neto', md5('neto'), 'A');
-insert into solicitante(nome, lotacao_id, cargo, telefone, email, login, senha, tipo_usuario) 
-    values('Guthierrez', 1, 'Estágiario', '(62) 4545-4554', 'guthierrez.gs.agr@gmail.com', 'guthierrez', 
-    md5('guthi123'), 'A');
+insert into solicitante(nome, lotacao_id, cargo, telefone, email, login, senha, perfil) 
+    values('Guthierrez', 1, 'Estágiario', '(62) 4545-4554', 'guthierrez.gs.agr@gmail.com', 
+    'guthierrez', md5('guthi123'), 'A');
