f8e087c2aaf40fc308d211f5439d117e84f37626
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-11-25 14:49:30 -0200

    Alteração do nome dos atributos.

--- static/js/models/Usuario.js
+++ static/js/models/Usuario.js
@@ -1,31 +1,31 @@
 function Usuario() {
-	this.id = null;
-	this.nome = "";
-	this.lotacao_id = "";
-	this.cargo = "";
-	this.telefone = "";
-	this.email = "";
-	this.login = "";
-	this.senha = "";
-	this.tipo_usuario = "";
-	this.status = "";
-	this.data_criacao = "";
-	this.data_alteracao = "";
+    this.id = null;
+    this.nome = "";
+    this.lotacao_id = "";
+    this.funcao = "";
+    this.telefone = "";
+    this.email = "";
+    this.login = "";
+    this.senha = "";
+    this.perfil = "";
+    this.status = "";
+    this.data_criacao = "";
+    this.data_alteracao = "";
 }
 
 Usuario.prototype.toJSON = function() {
-	return '{' +
-		'"id": ' + this.id + ', ' +
-    	'"nome": ' + '"' + this.nome + '", ' +
-    	'"lotacao_id": ' + this.lotacao_id + ', ' +
-    	'"cargo": ' + '"' + this.cargo + '", ' +
-    	'"telefone": ' + '"' + this.telefone + '", ' +
-    	'"email": ' + '"' + this.email + '", ' +
-    	'"login": ' + '"' + this.login + '", ' +
-    	'"senha": ' + '"' + this.senha + '", ' +
-    	'"tipo_usuario": ' + '"' + this.tipo_usuario + '", ' +
-    	'"status": ' + '"' + this.status + '", ' +
-    	'"data_criacao": ' + '"' + this.data_criacao + '", ' +
-    	'"data_alteracao": ' + '"' + this.data_alteracao + '"' +
+    return '{' +
+        '"id": ' + this.id + ', ' +
+        '"nome": ' + '"' + this.nome + '", ' +
+        '"lotacao_id": ' + this.lotacao_id + ', ' +
+        '"funcao": ' + '"' + this.funcao + '", ' +
+        '"telefone": ' + '"' + this.telefone + '", ' +
+        '"email": ' + '"' + this.email + '", ' +
+        '"login": ' + '"' + this.login + '", ' +
+        '"senha": ' + '"' + this.senha + '", ' +
+        '"perfil": ' + '"' + this.perfil + '", ' +
+        '"status": ' + '"' + this.status + '", ' +
+        '"data_criacao": ' + '"' + this.data_criacao + '", ' +
+        '"data_alteracao": ' + '"' + this.data_alteracao + '"' +
     '}';
 }
