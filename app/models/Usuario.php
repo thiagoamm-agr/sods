e6eea320dc8de5ecb591c136d8fd2dcc200e5904
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-11-25 14:51:49 -0200

    Alteração do nome dos atributos.

--- app/models/Usuario.php
+++ app/models/Usuario.php
@@ -1,18 +1,18 @@
 <?php
-	@require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/models/Model.php';
-	
-	class Usuario extends Model {				
-		protected $id;
-		protected $nome;
-		protected $lotacao_id;
-		protected $cargo;
-		protected $telefone;
-		protected $email;
-		protected $login;
-		protected $senha;
-		protected $tipo_usuario;
-		protected $status;
-		protected $data_criacao;
-		protected $data_alteracao;
-	}
+    @require_once $_SERVER['DOCUMENT_ROOT'] . '/sods/app/models/Model.php';
+    
+    class Usuario extends Model {
+        protected $id;
+        protected $nome;
+        protected $lotacao_id;
+        protected $funcao;
+        protected $telefone;
+        protected $email;
+        protected $login;
+        protected $senha;
+        protected $perfil;
+        protected $status;
+        protected $data_criacao;
+        protected $data_alteracao;
+    }
 ?>
