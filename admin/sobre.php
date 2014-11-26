43b6711d9f362b951275495eff69cfcab455b2eb
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-11-26 11:10:38 -0200

    Remoção do código <div class="col-md-12"> que deixava o conteúdo desalinhado em relação ao topo do sistema.

--- admin/sobre.php
+++ admin/sobre.php
@@ -2,34 +2,32 @@
     @include $_SERVER['DOCUMENT_ROOT'] . '/sods/includes/topo.php';
 ?>
          <div class="container">
-             <div class="col-md-12">
-                <div class="page-header">
-                    <h1>Sobre</h1>
-                </div>
-                <h3><strong>SODS </strong>-<span class="text-muted"> Sistema para Solicitação de Desenvolvimento de 
-                    Software</span></h3>
-                <p>&nbsp;</p>
-                <p>
-                    O <strong>SODS</strong> é um sistema concebido para controle das solicitações de desenvolvimento de 
-                    software na <strong>AGR</strong>.  
-                </p>
-                <p>
-                    Tem como intuito facilitar o processo de criação de demandas por parte das coordenações e gerências 
-                    e atendimento das mesmas pela equipe de desenvolvimento.
-                </p>
-                <p>Autores:</p>
-                <ul>
-                    <li>Thiago Alexandre Martins Monteiro</li>
-                    <li>Edward Neto</li>
-                    <li>Guthierrez Gregório de Souza</li>
-                    <li>Manoel Albino Neto</li>
-                </ul>
-             </div>
+            <div class="page-header">
+                <h1>Sobre</h1>
+            </div>
+            <h3><strong>SODS </strong>-<span class="text-muted"> Sistema para Solicitação de Desenvolvimento de 
+                Software</span></h3>
+            <p>&nbsp;</p>
+            <p>
+                O <strong>SODS</strong> é um sistema concebido para controle das solicitações de desenvolvimento de 
+                software na <strong>AGR</strong>.  
+            </p>
+            <p>
+                 Tem como intuito facilitar o processo de criação de demandas por parte das coordenações e gerências 
+                 e atendimento das mesmas pela equipe de desenvolvimento.
+            </p>
+            <p>Autores:</p>
+            <ul>
+                <li>Thiago Alexandre Martins Monteiro</li>
+                <li>Edward Neto</li>
+                <li>Guthierrez Gregório de Souza</li>
+                <li>Manoel Albino Neto</li>
+             </ul>
              <footer class="footer">
                 <div class="container">
                     <p class="text-muted" style="text-align:center">
                         <strong><a href="http://www.agr.go.gov.br/" target="blank" style="color:black">AGR</a></strong> 
-                        - Agência Goiana de Regulação, Controle e Fiscalização de Serviços Públicos &copy 2014
+                        - Agência Goiana de Regulação, Controle e Fiscalização de Serviços Públicos &copy; 2014
                     </p>
                 </div>
              </footer>
