6219718039f6721f042bc52117cef0ae0f2e18a4
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-11-29 18:20:06 -0200

    1 - Correção da lógica do login com a opção mantenha-me conectado. 2 - Refatoração do código.

--- index.php
+++ index.php
@@ -5,14 +5,28 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <link rel="stylesheet" type="text/css" media="screen" href="/sods/static/bootstrap/css/bootstrap.min.css" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Estilo para esse template -->
+        <link rel="stylesheet" type="text/css" media="screen" href="/sods/static/bootstrap/css/bootstrap.min.css" />
         <link href="/sods/static/bootstrap/css/signin.css" rel="stylesheet" />
 
+        <style type="text/css">
+            div.tooltip-inner {
+                max-width: 500px;
+            }
+        </style>
+
+        <script src="/sods/static/js/jquery.min.js"></script>
+        <script type="text/javascript" src="/sods/static/bootstrap/js/bootstrap.min.js"></script>
+        
         <title>Login</title>
+
+        <script type="text/javascript">
+            $(document).ready(function() {
+                $('[data-toggle="tooltip"]').tooltip({'placement': 'bottom'});
+            });
+        </script>
     </head>
         <body>
             <center><h2><b>SODS</b> - Sistema de Solicitação de Desenvolvimento de Software</h2></center>
@@ -22,12 +36,18 @@
                         <input type="text" id="login" name="login" 
                             onblur="if(this.value == '<?php echo isset($_COOKIE['login'])? $_COOKIE['login']: '' ?>')
                             senha.value = '<?php echo isset($_COOKIE['senha'])? $_COOKIE['senha']: '' ?>'"  
-                            class="form-control" placeholder="Login" required autofocus />
+                            class="form-control" placeholder="Usuário" required autofocus />
                         <input type="password" id="senha" name="senha" class="form-control" 
                             placeholder="Senha" required />
                         <div class="checkbox">
                             <label>
-                                <input type="checkbox" id="lembrar" name="lembrar"> Continue conectado por uma semana
+                                <input type="checkbox" id="lembrar" name="lembrar">
+                                    <span 
+                                        data-toggle="tooltip" 
+                                        data-placement="bottom" 
+                                        title="Habilita o login automático durante uma semana.">
+                                        Mantenha-me conectado.
+                                    </span>
                             </label>
                         </div>
                         <button class="btn btn-lg btn-primary btn-block" type="submit">
@@ -42,35 +62,34 @@
     $senha = isset($_POST['senha']) ? $_POST['senha'] : '';
     $lembrar = isset($_POST['lembrar']) ? $_POST['lembrar'] : '';
 
-    // Calcula tempo de armazenamento do cookie.
-    $duracao_cookie = time() + 3600 * 24 * 7; // 1 semana.
-    $_SESSION['duracao_cookie'] = $duracao_cookie;
-
-    // Lembra dos dados de acesso.
-    if (!empty($lembrar)) {
-        // Configura os valores do cookie.
-        setcookie('login', $login, $duracao_cookie);
-        setcookie('senha', $senha, $duracao_cookie);
-        $_COOKIE['login'] = $login;
-        $_COOKIE['senha'] = $senha;
-        $_SESSION['remember_me'] = true;
-    } else {
-        // Limpa resíduos de cookie.
-        setcookie('login', '', $duracao_cookie);
-        setcookie('senha', '', $duracao_cookie);
-        $_COOKIE['login'] = '';
-        $_COOKIE['senha'] = '';
-        $_SESSION['remember_me'] = false;
+    if (!isset($_COOKIE['lembrar'])) {
+        if (!empty($lembrar)) {
+            // Define que os cookies irão durar uma semana a partir de agora.
+            $duracao = time() + 3600 * 24 * 7;
+            $path = '/sods';
+            $domain = null;
+            $secure = false;
+            $http_only = true;
+            setcookie('lembrar', true, $duracao, $path, $domain, $secure, $http_only);
+            setcookie('login', $login, $duracao, $path, $domain, $secure, $http_only);
+            setcookie('senha', $senha, $duracao, $path, $domain, $secure, $http_only);
+            setcookie('duracao', $duracao, $duracao, $path, $domain, $secure, $http_only);
+            $_COOKIE['lembrar'] = true;
+            $_COOKIE['login'] = $login;
+            $_COOKIE['senha'] = $senha;
+            $_COOKIE['duracao'] = $duracao;
+        }
     }
-    if (isset($_COOKIE['login']) && isset($_COOKIE['senha'])) {
+    if (isset($_COOKIE['lembrar'])) {
         $login = $_COOKIE['login'];
         $senha = $_COOKIE['senha'];
         if (autenticar_usuario($login, $senha)) {
-            // Redireciona para a área administrativa.
             header('location: /sods/admin');
         }
     } else {
-        if (autenticar_usuario($login, $senha)) {
+        if (isset($_SESSION['usuario'])) {
+            header('location: /sods/admin');
+        } else if (autenticar_usuario($login, $senha)) {
             header('location: /sods/admin');
         } else if (isset($_POST['login']) && isset($_POST['senha'])) {
 ?>
