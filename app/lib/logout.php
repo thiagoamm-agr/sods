deba7b36a3233f08fb96fc3d8daee82386628171
Thiago Monteiro <thiago.amm.agr@gmail.com>
2014-11-29 18:15:37 -0200

    Aplicada correção na destruição dos cookies.

--- app/lib/logout.php
+++ app/lib/logout.php
@@ -7,21 +7,28 @@
     // Protege o script de acesso direto.
     script_guard();
 
-    // Destruindo a sessão e os cookies.
-    if (isset($_COOKIE[session_name()])) {
-        setcookie(session_name(), '');
+    // Exclui os cookies.
+    if (isset($_COOKIE['lembrar'])) {
+        $duracao = time() - 3600;
+        $path = '/sods';
+        $domain = null;
+        $secure = false;
+        $http_only = true;
+        // Arquivo de cookie do navegador.
+        setcookie('lembrar', '', $duracao, $path, $domain, $secure, $http_only);
+        setcookie('login', '', $duracao, $path, $domain, $secure, $http_only);
+        setcookie('senha', '', $duracao, $path, $domain, $secure, $http_only);
+        setcookie('duracao', '', $duracao, $path, $domain, $secure, $http_only);
+        // Memória.
+        unset($_COOKIE['lembrar']);
+        unset($_COOKIE['login']);
+        unset($_COOKIE['senha']);
+        unset($_COOKIE['duracao']);
     }
 
-    if (isset($_COOKIE['login'])) {
-        setcookie('login', '');
-    }
-
-    if (isset($_COOKIE['senha'])) {
-        setcookie('senha', '');
-    }
-
+    // Destrói a sessão.
     session_unset();
     session_destroy();
 
-    header('location: /sods/index.php');
+    header('location: /sods/');
 ?>
